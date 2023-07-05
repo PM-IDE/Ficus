5dbb1f5fab2f3c28a5aae85ae4622d4452931d68
aero <aerooneQ@yandex.ru>
2023-07-06 01:25:30 +0300

    Refacorings

--- src/rust/ficus_backend/src/event_log/file_xes_log_reader.rs
+++ src/rust/ficus_backend/src/event_log/file_xes_log_reader.rs
@@ -1,6 +1,6 @@
 use std::{rc::Rc, cell::RefCell, io::BufReader, fs::File, collections::HashMap, str::FromStr};
 use chrono::{Utc, DateTime};
-use quick_xml::Reader;
+use quick_xml::{Reader, events::BytesStart};
 
 use super::event::{EventImpl, Lifecycle, EventPayloadValue, XesStandardLifecycle};
 
@@ -62,6 +62,8 @@ impl Iterator for TraceXesEventLogIterator {
     }
 }
 
+struct KeyValuePair<TKey, TValue> { pub key: Option<TKey>, pub value: Option<TValue> }
+
 impl TraceXesEventLogIterator {
     fn new(reader: Rc<RefCell<Reader<BufReader<File>>>>) -> TraceXesEventLogIterator {
         TraceXesEventLogIterator { reader, buffer: Vec::new() }
@@ -88,88 +90,108 @@ impl TraceXesEventLogIterator {
                         _ => continue
                     }
                 },
-                Ok(quick_xml::events::Event::Empty(start)) => {
-                    let mut key: Option<String> = None;
-                    let mut value: Option<String> = None;
-                    let mut payload_value: Option<EventPayloadValue> = None;
+                Ok(quick_xml::events::Event::Empty(empty)) => {
+                    let kv = Self::extract_key_value(&empty);
+                    if !kv.value.is_some() || !kv.key.is_some() { return None }
 
-                    for attr in start.attributes() {
-                        match attr {
-                            Err(_) => return None,
-                            Ok(real_attr) => {
-                                match real_attr.key.0 {
-                                    KEY_ATTR_NAME => match String::from_utf8(real_attr.value.to_owned().to_vec()) {
-                                        Err(_) => return None,
-                                        Ok(string) => key = Some(string)
-                                    },
-                                    VALUE_ATTR_NAME => match String::from_utf8(real_attr.value.to_owned().to_vec()) {
-                                        Err(_) => return None,
-                                        Ok(string) => value = Some(string)
-                                    },
-                                    _ => continue
-                                }
-                            }
-                        }
-                    }
+                    let key = kv.key.as_ref().unwrap().as_str();
+                    let value = kv.value.as_ref().unwrap().as_str();
 
-                    if !value.is_some() || !key.is_some() { return None }
+                    let payload_value = Self::extract_payload_value(&empty, value);
+                    if !payload_value.is_some() { return None }
+                    Self::update_event_data(key, payload_value.unwrap(), &mut date, &mut name, &mut lifecycle, &payload)
+                },
+                _ => continue
+            }
+        }
+    }
 
-                    match start.name().0 {
-                        DATE_TAG_NAME => {
-                            match DateTime::parse_from_rfc3339(value.unwrap().as_str()) {
-                                Err(_) => return None,
-                                Ok(date) => payload_value = Some(EventPayloadValue::Date(date.with_timezone(&Utc)))
-                            }
+    fn extract_key_value(start: &BytesStart) -> KeyValuePair<String, String> {
+        let mut key: Option<String> = None;
+        let mut value: Option<String> = None;
+
+        for attr in start.attributes() {
+            match attr {
+                Err(_) => continue,
+                Ok(real_attr) => {
+                    match real_attr.key.0 {
+                        KEY_ATTR_NAME => match String::from_utf8(real_attr.value.to_owned().to_vec()) {
+                            Err(_) => continue,
+                            Ok(string) => key = Some(string)
                         },
-                        INT_TAG_NAME => {
-                            match value.unwrap().parse::<i32>() {
-                                Err(_) => return None,
-                                Ok(int_value) => payload_value = Some(EventPayloadValue::Int(int_value))
-                            }
-                        },
-                        FLOAT_TAG_NAME => {
-                            match value.unwrap().parse::<f32>() {
-                                Err(_) => return None,
-                                Ok(float_value) => payload_value = Some(EventPayloadValue::Float(float_value))
-                            }
-                        },
-                        STRING_TAG_NAME => {
-                            payload_value = Some(EventPayloadValue::String(value.unwrap()))
-                        },
-                        BOOLEAN_TAG_NAME => {
-                            match value.unwrap().parse::<bool>() {
-                                Err(_) => return None,
-                                Ok(bool_value) => payload_value = Some(EventPayloadValue::Boolean(bool_value))
-                            }
+                        VALUE_ATTR_NAME => match String::from_utf8(real_attr.value.to_owned().to_vec()) {
+                            Err(_) => continue,
+                            Ok(string) => value = Some(string)
                         },
                         _ => continue
                     }
+                }
+            }
+        }
 
-                    match key.as_ref().unwrap().as_str() {
-                        TIME_TIMESTAMP => {
-                            if let Some(EventPayloadValue::Date(parsed_date)) = payload_value {
-                                date = Some(parsed_date);
-                            }
-                        },
-                        CONCEPT_NAME => {
-                            if let Some(EventPayloadValue::String(parsed_string)) = payload_value {
-                                name = Some(parsed_string);
-                            }
-                        },
-                        LIFECYCLE_TRANSITION => {
-                            if let Some(EventPayloadValue::String(parsed_string)) = payload_value {
-                                match XesStandardLifecycle::from_str(parsed_string.as_str()) {
-                                    Ok(lifecycle_value) => lifecycle = Some(Lifecycle::XesStandardLifecycle(lifecycle_value)),
-                                    Err(_) => return None
-                                }
-                            }
-                        },
-                        _ => {
-                            payload.borrow_mut().insert(key.unwrap(), payload_value.unwrap());
-                        }
+        return KeyValuePair { key, value };
+    }
+
+    fn extract_payload_value(empty: &BytesStart, value: &str) -> Option<EventPayloadValue> {
+        match empty.name().0 {
+            DATE_TAG_NAME => {
+                match DateTime::parse_from_rfc3339(value) {
+                    Err(_) => None,
+                    Ok(date) => Some(EventPayloadValue::Date(date.with_timezone(&Utc)))
+                }
+            },
+            INT_TAG_NAME => {
+                match value.parse::<i32>() {
+                    Err(_) => None,
+                    Ok(int_value) => Some(EventPayloadValue::Int(int_value))
+                }
+            },
+            FLOAT_TAG_NAME => {
+                match value.parse::<f32>() {
+                    Err(_) => None,
+                    Ok(float_value) => Some(EventPayloadValue::Float(float_value))
+                }
+            },
+            STRING_TAG_NAME => {
+                Some(EventPayloadValue::String(value.to_owned()))
+            },
+            BOOLEAN_TAG_NAME => {
+                match value.parse::<bool>() {
+                    Err(_) => None,
+                    Ok(bool_value) => Some(EventPayloadValue::Boolean(bool_value))
+                }
+            },
+            _ => None
+        }
+    }
+
+    fn update_event_data(key: &str,
+                         payload_value: EventPayloadValue,
+                         date: &mut Option<DateTime<Utc>>,
+                         name: &mut Option<String>,
+                         lifecycle: &mut Option<Lifecycle>,
+                         payload: &Rc<RefCell<HashMap<String, EventPayloadValue>>>) {
+        match key {
+            TIME_TIMESTAMP => {
+                if let EventPayloadValue::Date(parsed_date) = payload_value {
+                    *date = Some(parsed_date);
+                }
+            },
+            CONCEPT_NAME => {
+                if let EventPayloadValue::String(parsed_string) = payload_value {
+                    *name = Some(parsed_string);
+                }
+            },
+            LIFECYCLE_TRANSITION => {
+                if let EventPayloadValue::String(parsed_string) = payload_value {
+                    match XesStandardLifecycle::from_str(parsed_string.as_str()) {
+                        Ok(lifecycle_value) => *lifecycle = Some(Lifecycle::XesStandardLifecycle(lifecycle_value)),
+                        _ => {}
                     }
-                },
-                _ => continue
+                }
+            },
+            _ => {
+                payload.borrow_mut().insert(key.to_owned(), payload_value);
             }
         }
     }
