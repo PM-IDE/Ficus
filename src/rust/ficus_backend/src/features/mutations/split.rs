c7401189a1e9412e8156aea6365899727ac26b5b
aero <aerooneQ@yandex.ru>
2023-07-19 01:25:52 +0300

    More deterministic split

--- src/rust/ficus_backend/src/features/mutations/split.rs
+++ src/rust/ficus_backend/src/features/mutations/split.rs
@@ -7,20 +7,32 @@ use std::{
 
 use crate::event_log::core::{event::Event, event_log::EventLog, trace::Trace};
 
+struct TracePointer<TTrace> {
+    pub trace: Rc<RefCell<TTrace>>,
+    pub index: usize,
+}
+
+impl<TTrace> TracePointer<TTrace> {
+    pub fn new(trace: Rc<RefCell<TTrace>>, index: usize) -> TracePointer<TTrace> {
+        TracePointer { trace, index }
+    }
+}
+
+impl<TTrace> Clone for TracePointer<TTrace> {
+    fn clone(&self) -> Self {
+        Self {
+            trace: Rc::clone(&self.trace),
+            index: self.index,
+        }
+    }
+}
+
 pub fn split_by_traces<TLog, TTrace>(log: &TLog) -> Vec<Vec<Rc<RefCell<TTrace>>>>
 where
     TLog: EventLog<TTrace = TTrace>,
     TTrace: Trace,
 {
-    let mut len_to_traces: HashMap<usize, Vec<Rc<RefCell<TTrace>>>> = HashMap::new();
-    for trace in log.get_traces() {
-        let len = trace.borrow().get_events().len();
-        if len_to_traces.contains_key(&len) {
-            (*len_to_traces.get_mut(&len).unwrap()).push(Rc::clone(trace));
-        } else {
-            len_to_traces.insert(len, vec![Rc::clone(trace)]);
-        }
-    }
+    let len_to_traces = create_len_to_traces_map(log);
 
     let mut result = Vec::new();
     for (_, traces) in len_to_traces {
@@ -29,44 +41,15 @@ where
             continue;
         }
 
-        let mut groups = Vec::new();
-        for trace in &traces {
-            groups.push(Rc::clone(&trace));
-        }
-
-        let mut groups = vec![groups];
-
+        let mut groups = create_initial_groups(&traces);
         let mut index = 0;
+
         loop {
-            if index >= traces[0].borrow().get_events().len() {
+            if index >= traces[0].trace.borrow().get_events().len() {
                 break;
             }
 
-            let mut new_groups = Vec::new();
-            let mut all_groups_have_one_trace = true;
-            for group in &groups {
-                if group.len() == 1 {
-                    new_groups.push(group.to_vec());
-                    continue;
-                }
-
-                all_groups_have_one_trace = false;
-                let mut hashes_to_traces: HashMap<u64, Vec<Rc<RefCell<TTrace>>>> = HashMap::new();
-                for trace in group {
-                    let mut hasher = DefaultHasher::new();
-                    trace.borrow().get_events()[index].borrow().get_name().hash(&mut hasher);
-                    let hash_code = hasher.finish();
-                    if hashes_to_traces.contains_key(&hash_code) {
-                        (*hashes_to_traces.get_mut(&hash_code).unwrap()).push(Rc::clone(&trace));
-                    } else {
-                        hashes_to_traces.insert(hash_code, vec![Rc::clone(&trace)]);
-                    }
-                }
-
-                for (_, new_group) in hashes_to_traces {
-                    new_groups.push(new_group);
-                }
-            }
+            let (new_groups, all_groups_have_one_trace) = update_groups(&groups, index);
 
             if all_groups_have_one_trace {
                 break;
@@ -81,5 +64,82 @@ where
         }
     }
 
+    sort_resulting_variants(result)
+}
+
+fn create_len_to_traces_map<TLog, TTrace>(log: &TLog) -> HashMap<usize, Vec<TracePointer<TTrace>>>
+where
+    TLog: EventLog<TTrace = TTrace>,
+    TTrace: Trace,
+{
+    let mut len_to_traces: HashMap<usize, Vec<TracePointer<TTrace>>> = HashMap::new();
+    let traces = log.get_traces();
+
+    for index in 0..traces.len() {
+        let trace = Rc::clone(&traces[index]);
+        let len = trace.borrow().get_events().len();
+        if len_to_traces.contains_key(&len) {
+            (*len_to_traces.get_mut(&len).unwrap()).push(TracePointer::new(trace, index));
+        } else {
+            len_to_traces.insert(len, vec![TracePointer::new(trace, index)]);
+        }
+    }
+
+    len_to_traces
+}
+
+fn create_initial_groups<TTrace>(traces: &Vec<TracePointer<TTrace>>) -> Vec<Vec<TracePointer<TTrace>>> {
+    let mut groups = Vec::new();
+    for trace in traces {
+        groups.push((*trace).clone());
+    }
+
+    vec![groups]
+}
+
+fn update_groups<TTrace>(
+    groups: &Vec<Vec<TracePointer<TTrace>>>,
+    index: usize,
+) -> (Vec<Vec<TracePointer<TTrace>>>, bool)
+where
+    TTrace: Trace,
+{
+    let mut new_groups = Vec::new();
+    let mut all_groups_have_one_trace = true;
+    for group in groups {
+        if group.len() == 1 {
+            new_groups.push(group.to_vec());
+            continue;
+        }
+
+        all_groups_have_one_trace = false;
+        let mut hashes_to_traces: HashMap<u64, Vec<TracePointer<TTrace>>> = HashMap::new();
+        for trace in group {
+            let mut hasher = DefaultHasher::new();
+            let actual_trace = trace.trace.borrow();
+            let event = actual_trace.get_events()[index].borrow();
+            event.get_name().hash(&mut hasher);
+
+            let hash_code = hasher.finish();
+            if hashes_to_traces.contains_key(&hash_code) {
+                (*hashes_to_traces.get_mut(&hash_code).unwrap()).push((*trace).clone());
+            } else {
+                hashes_to_traces.insert(hash_code, vec![(*trace).clone()]);
+            }
+        }
+
+        for (_, new_group) in hashes_to_traces {
+            new_groups.push(new_group);
+        }
+    }
+
+    (new_groups, all_groups_have_one_trace)
+}
+
+fn sort_resulting_variants<TTrace>(mut result: Vec<Vec<TracePointer<TTrace>>>) -> Vec<Vec<Rc<RefCell<TTrace>>>> {
+    result.sort_by(|first, second| first[0].index.cmp(&second[0].index));
     result
+        .into_iter()
+        .map(|group| group.into_iter().map(|ptr| ptr.trace).collect())
+        .collect()
 }
