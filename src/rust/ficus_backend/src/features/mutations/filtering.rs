e683db0f86037128e121d6b7373f73c3095cfcfe
aero <aerooneQ@yandex.ru>
2023-07-18 14:58:43 +0300

    Add some functions + tests

--- src/rust/ficus_backend/src/features/mutations/filtering.rs
+++ src/rust/ficus_backend/src/features/mutations/filtering.rs
@@ -1,8 +1,17 @@
-use crate::event_log::core::event_log::EventLog;
+use std::collections::HashSet;
 
-fn filter_log_by_name<T>(log: &T)
+use crate::event_log::core::{event::Event, event_log::EventLog};
+
+pub fn filter_log_by_name<TLog>(log: &mut TLog, name: &str)
 where
-    T: EventLog,
+    TLog: EventLog,
 {
-    for trace in log.get_traces() {}
+    log.filter_events_by(|event| event.get_name() == name);
+}
+
+pub fn filter_log_by_names<TLog>(log: &mut TLog, names: &HashSet<String>)
+where
+    TLog: EventLog,
+{
+    log.filter_events_by(|event| names.contains(event.get_name()));
 }
--- src/rust/ficus_backend/tests/mutations_tests.rs
+++ src/rust/ficus_backend/tests/mutations_tests.rs
@@ -1,5 +1,10 @@
-use ficus_backend::event_log::core::{event::Event, event_log::EventLog};
+use std::{collections::HashSet, vec};
 use test_core::simple_events_logs_provider::create_simple_event_log;
+
+use ficus_backend::{
+    event_log::core::{event::Event, event_log::EventLog},
+    features::mutations::filtering::{filter_log_by_name, filter_log_by_names},
+};
 
 mod test_core;
 
@@ -17,4 +22,30 @@ fn test_removing_events2() {
     log.filter_events_by(|event| event.get_name() == "B" || event.get_name() == "C");
 
     assert_eq!(log.to_raw_vector(), vec![vec!["A"], vec!["A"]]);
+}
+
+#[test]
+fn test_removing_events3() {
+    let mut log = create_simple_event_log();
+    filter_log_by_name(&mut log, "A");
+
+    assert_eq!(log.to_raw_vector(), vec![vec!["B", "C"], vec!["B", "C"]]);
+}
+
+#[test]
+fn test_removing_events4() {
+    let mut log = create_simple_event_log();
+    let set = HashSet::from_iter(vec!["A".to_string(), "B".to_string()]);
+    filter_log_by_names(&mut log, &set);
+
+    assert_eq!(log.to_raw_vector(), vec![vec!["C"], vec!["C"]]);
+}
+
+#[test]
+fn test_removing_events5() {
+    let mut log = create_simple_event_log();
+    let set = HashSet::from_iter(vec!["A".to_string(), "B".to_string(), "C".to_string()]);
+    filter_log_by_names(&mut log, &set);
+
+    assert!(log.to_raw_vector().is_empty());
 }
