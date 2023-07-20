7c029a3d3c702851a844962c07076c782569fe04
aero <aerooneQ@yandex.ru>
2023-07-20 14:51:09 +0300

    Started adding proper test

--- src/rust/ficus_backend/tests/analysis_tests.rs
+++ src/rust/ficus_backend/tests/analysis_tests.rs
@@ -1,6 +1,13 @@
-use ficus_backend::features::analysis::event_log_info::EventLogInfo;
+use std::collections::HashMap;
 
-use crate::test_core::simple_events_logs_provider::create_simple_event_log;
+use ficus_backend::features::analysis::{
+    dfg_entropy::{calculate_default_dfg_entropy, calculate_laplace_dfg_entropy},
+    event_log_info::EventLogInfo,
+};
+
+use crate::test_core::simple_events_logs_provider::{
+    create_log_from_filter_out_chaotic_events, create_simple_event_log,
+};
 
 mod test_core;
 
@@ -27,4 +34,32 @@ fn test_dfg_info() {
     assert_eq!(followers.get(&"C".to_string()).unwrap(), &2usize);
 
     assert_eq!(dfg.get_followed_events(&"C".to_string()), None);
+}
+
+#[test]
+fn test_dfg_entropy() {
+    let log = create_log_from_filter_out_chaotic_events();
+    let entropies = calculate_default_dfg_entropy(&log);
+    let expected = HashMap::from_iter(vec![
+        ("c".to_string(), 1.8365916681089791),
+        ("b".to_string(), 1.8365916681089791),
+        ("x".to_string(), 3.169925001442312),
+        ("a".to_string(), 0.9182958340544896),
+    ]);
+
+    assert_eq!(entropies, expected);
+}
+
+#[test]
+fn test_dfg_laplace_entropy() {
+    let log = create_log_from_filter_out_chaotic_events();
+    let entropies = calculate_laplace_dfg_entropy(&log);
+    let expected = HashMap::from_iter(vec![
+        ("c".to_string(), 1.905904975406124),
+        ("b".to_string(), 1.905904975406124),
+        ("x".to_string(), 3.2127002996007796),
+        ("a".to_string(), 1.002726083454847),
+    ]);
+
+    assert_eq!(entropies, expected);
 }
--- src/rust/ficus_backend/tests/event_log_info_tests.rs
+++ src/rust/ficus_backend/tests/event_log_info_tests.rs
@@ -15,15 +15,3 @@ fn test_event_log_info() {
     assert_eq!(log_info.get_event_count(&"B".to_string()), 2usize);
     assert_eq!(log_info.get_event_count(&"C".to_string()), 2usize);
 }
-
-#[test]
-fn test_dfg_entropy() {
-    let log = create_simple_event_log();
-    println!("{:?}", calculate_default_dfg_entropy(&log));
-}
-
-#[test]
-fn test_dfg_entropy_log_from_paper() {
-    let log = create_log_from_filter_out_chaotic_events();
-    println!("{:?}", calculate_default_dfg_entropy(&log));
-}
