653250b31aeb81542ccd7e280e2ee4df53ed2434
aero <aerooneQ@yandex.ru>
2023-07-22 03:30:04 +0300

    Small refactorings

--- src/rust/ficus_backend/tests/patterns_tests.rs
+++ src/rust/ficus_backend/tests/patterns_tests.rs
@@ -13,12 +13,11 @@ fn test_tandem_arrays_from_paper() {
     let log = create_log_from_taxonomy_of_patterns();
     let hashes = log.to_hashes_event_log::<NameEventHasher>();
     let tandem_arrays = find_maximal_tandem_arrays(&hashes, 10);
-    let tuples = tandem_arrays[0]
-        .iter()
-        .map(|array| to_tuple(array))
-        .collect::<Vec<(usize, usize, usize)>>();
 
-    assert_eq!(tuples, [(2, 3, 4), (3, 3, 4), (4, 3, 3), (2, 6, 2), (3, 6, 2)]);
+    assert_eq!(
+        get_first_trace_tuples(tandem_arrays),
+        [(2, 3, 4), (3, 3, 4), (4, 3, 3), (2, 6, 2), (3, 6, 2)]
+    );
 }
 
 fn to_tuple(array: &TandemArrayInfo) -> (usize, usize, usize) {
@@ -29,17 +28,17 @@ fn to_tuple(array: &TandemArrayInfo) -> (usize, usize, usize) {
     )
 }
 
+fn get_first_trace_tuples(tandem_arrays: Vec<Vec<TandemArrayInfo>>) -> Vec<(usize, usize, usize)> {
+    tandem_arrays[0].iter().map(|array| to_tuple(array)).collect()
+}
+
 #[test]
 fn test_no_tandem_arrays() {
     let log = create_no_tandem_array_log();
     let hashes = log.to_hashes_event_log::<NameEventHasher>();
     let tandem_arrays = find_maximal_tandem_arrays(&hashes, 10);
-    let tuples = tandem_arrays[0]
-        .iter()
-        .map(|array| to_tuple(array))
-        .collect::<Vec<(usize, usize, usize)>>();
 
-    assert_eq!(tuples, []);
+    assert_eq!(get_first_trace_tuples(tandem_arrays), []);
 }
 
 #[test]
@@ -47,10 +46,6 @@ fn test_one_tandem_array() {
     let log = create_one_tandem_array_log();
     let hashes = log.to_hashes_event_log::<NameEventHasher>();
     let tandem_arrays = find_maximal_tandem_arrays(&hashes, 10);
-    let tuples = tandem_arrays[0]
-        .iter()
-        .map(|array| to_tuple(array))
-        .collect::<Vec<(usize, usize, usize)>>();
 
-    assert_eq!(tuples, [(0, 2, 2)]);
+    assert_eq!(get_first_trace_tuples(tandem_arrays), [(0, 2, 2)]);
 }
