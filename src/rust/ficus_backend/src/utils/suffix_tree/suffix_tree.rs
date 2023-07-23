46d398c9df13a5d0c48164c1f35d4e31414fc9ee
aero <aerooneQ@yandex.ru>
2023-07-24 00:24:50 +0300

    Support finding patterns

--- src/rust/ficus_backend/src/utils/suffix_tree/suffix_tree.rs
+++ src/rust/ficus_backend/src/utils/suffix_tree/suffix_tree.rs
@@ -30,6 +30,71 @@ where
         maximal_repeats
     }
 
+    pub fn find_patterns(&self, pattern: &[TElement]) -> Option<Vec<(usize, usize)>> {
+        let mut current_node_index = 0;
+        let mut pattern_index = 0;
+        let mut suffix_length = 0;
+
+        loop {
+            if pattern_index == pattern.len() {
+                break;
+            }
+
+            let current_node = &self.nodes[current_node_index];
+            if !current_node.children.contains_key(&Some(pattern[pattern_index])) {
+                return None;
+            }
+
+            let child_index = current_node.children.get(&Some(pattern[pattern_index])).unwrap();
+            let child_node = self.nodes.get(*child_index).unwrap();
+
+            for i in child_node.left..child_node.right {
+                if pattern_index == pattern.len() {
+                    break;
+                }
+
+                let slice_element = self.slice.get(i);
+                if slice_element.is_none() || slice_element.unwrap() != pattern[pattern_index] {
+                    return None;
+                }
+
+                pattern_index += 1;
+            }
+
+            current_node_index = *child_index;
+            suffix_length += child_node.edge_len();
+        }
+
+        let mut patterns = Vec::new();
+
+        suffix_length -= &self.nodes[current_node_index].edge_len();
+        self.dfs_pattern_search(current_node_index, &mut patterns, pattern.len(), suffix_length);
+
+        Some(patterns)
+    }
+
+    fn dfs_pattern_search(
+        &self,
+        index: usize,
+        patterns: &mut Vec<(usize, usize)>,
+        pattern_length: usize,
+        mut suffix_length: usize,
+    ) {
+        let node = self.nodes.get(index).unwrap();
+        suffix_length += node.edge_len();
+        
+        let left = node.right - suffix_length;
+        patterns.push((left, left + pattern_length));
+
+        if node.is_leaf() {
+            return;
+        }
+
+        for (_, child_node_index) in &node.children {
+            self.dfs_pattern_search(*child_node_index, patterns, pattern_length, suffix_length);
+        }
+    }
+
     fn dfs(
         &self,
         index: usize,
