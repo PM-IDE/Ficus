ef5817690c316ac770835aca9a790f3878a858eb
aero <aerooneQ@yandex.ru>
2023-07-27 04:19:30 +0300

    Implementation of activities instances extraction

--- src/rust/ficus_backend/src/features/analysis/patterns/repeat_sets.rs
+++ src/rust/ficus_backend/src/features/analysis/patterns/repeat_sets.rs
@@ -1,6 +1,6 @@
 use std::{
     cell::RefCell,
-    collections::{HashMap, HashSet},
+    collections::{HashMap, HashSet, VecDeque},
     rc::Rc,
 };
 
@@ -180,4 +180,184 @@ pub fn build_repeat_set_tree_from_repeats(
     }
 
     top_level_nodes
+}
+
+pub struct ActivityInTraceInfo {
+    pub node: Rc<RefCell<ActivityNode>>,
+    pub start_pos: usize,
+    pub length: usize,
+}
+
+pub fn extract_activities_instances(
+    log: &Vec<Vec<u64>>,
+    activities: &mut Vec<Rc<RefCell<ActivityNode>>>,
+    should_narrow: bool,
+) -> Vec<Vec<ActivityInTraceInfo>> {
+    let activities_by_size = split_activities_nodes_by_size(activities);
+    let mut result = vec![];
+
+    for trace in log {
+        let mut trace_activities = vec![];
+        let mut index = None;
+        let mut current_activity = None;
+        let mut last_activity_start_index = None;
+        let mut current_event_classes = HashSet::new();
+
+        while index.is_none() || index.unwrap() < trace.len() {
+            if index.is_none() {
+                index = Some(0);
+            } else {
+                *index.as_mut().unwrap() += 1;
+            }
+
+            if index.unwrap() >= trace.len() {
+                break;
+            }
+
+            let event_hash = trace[index.unwrap()];
+            if current_activity.is_none() {
+                let mut found_activity = false;
+                for activities in &activities_by_size {
+                    for activity in activities {
+                        if activity.borrow().event_classes.contains(&event_hash) {
+                            current_activity = Some(Rc::clone(activity));
+                            last_activity_start_index = Some(index.unwrap());
+                            found_activity = true;
+                            break;
+                        }
+                    }
+
+                    if found_activity {
+                        current_event_classes.clear();
+                        current_event_classes.insert(event_hash);
+                        break;
+                    }
+                }
+
+                continue;
+            }
+
+            if current_activity.as_ref().unwrap().borrow().event_classes.contains(&event_hash) {
+                let mut new_set = current_event_classes.clone();
+                new_set.insert(event_hash);
+
+                let mut found_new_set = false;
+                for activities_set in &activities_by_size {
+                    if activities_set.len() == 0
+                        || activities_set[0].borrow().len() < current_activity.as_ref().unwrap().borrow().len()
+                    {
+                        continue;
+                    }
+
+                    for activity in activities_set {
+                        if new_set.is_subset(&activity.borrow().event_classes) {
+                            current_activity = Some(Rc::clone(activity));
+                            found_new_set = true;
+                            break;
+                        }
+                    }
+
+                    if found_new_set {
+                        current_event_classes.insert(event_hash);
+                        break;
+                    }
+                }
+
+                if !found_new_set {
+                    if should_narrow {
+                        let activity = narrow_activity(current_activity.as_ref().unwrap(), &current_event_classes);
+                        current_activity = Some(activity);
+                    }
+
+                    let activity_instance = ActivityInTraceInfo {
+                        node: Rc::clone(current_activity.as_ref().unwrap()),
+                        start_pos: last_activity_start_index.unwrap(),
+                        length: index.unwrap() - last_activity_start_index.unwrap(),
+                    };
+
+                    trace_activities.push(activity_instance);
+
+                    current_activity = None;
+                    current_event_classes.clear();
+                    last_activity_start_index = None;
+                    index = None;
+                }
+            } else {
+                current_event_classes.insert(event_hash);
+            }
+        }
+
+        if last_activity_start_index.is_some() {
+            if should_narrow {
+                let activity = narrow_activity(current_activity.as_ref().unwrap(), &current_event_classes);
+                current_activity = Some(activity);
+            }
+
+            let activity_instance = ActivityInTraceInfo {
+                node: Rc::clone(current_activity.as_ref().unwrap()),
+                start_pos: last_activity_start_index.unwrap(),
+                length: index.unwrap() - last_activity_start_index.unwrap(),
+            };
+
+            trace_activities.push(activity_instance);
+        }
+
+        result.push(trace_activities);
+    }
+
+    result
+}
+
+fn split_activities_nodes_by_size(
+    activities: &mut Vec<Rc<RefCell<ActivityNode>>>,
+) -> Vec<Vec<Rc<RefCell<ActivityNode>>>> {
+    if activities.is_empty() {
+        return vec![];
+    }
+
+    activities.sort_by(|first, second| first.borrow().len().cmp(&second.borrow().len()));
+    let mut current_length = activities[0].borrow().len();
+    let mut result = vec![vec![Rc::clone(activities.get(0).unwrap())]];
+
+    for activity in activities.iter() {
+        if activity.borrow().len() != current_length {
+            result.push(vec![]);
+            current_length = activity.borrow().len();
+        }
+
+        result.last_mut().unwrap().push(Rc::clone(activity));
+    }
+
+    result
+}
+
+fn narrow_activity(node_ptr: &Rc<RefCell<ActivityNode>>, activities_set: &HashSet<u64>) -> Rc<RefCell<ActivityNode>> {
+    let mut q = VecDeque::new();
+    let node = node_ptr.borrow();
+    for child in &node.children {
+        q.push_back(Rc::clone(child));
+    }
+
+    let mut result = vec![];
+    while !q.is_empty() {
+        let current_activity_ptr = q.pop_front().unwrap();
+        let current_activity = current_activity_ptr.borrow();
+
+        if current_activity.event_classes.is_superset(&activities_set) {
+            result.push(Rc::clone(&current_activity_ptr));
+            for child_node in &current_activity.children {
+                q.push_back(Rc::clone(child_node));
+            }
+        }
+    }
+
+    if result.is_empty() {
+        return Rc::clone(&node_ptr);
+    }
+
+    let result = result
+        .iter()
+        .max_by(|first, second| first.borrow().len().cmp(&second.borrow().len()));
+
+    Rc::clone(result.unwrap())
 }
