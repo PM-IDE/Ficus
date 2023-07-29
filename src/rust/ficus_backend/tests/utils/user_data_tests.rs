14479e1ab9969c095c07acd1a5405aad0481cb1c
aero <aerooneQ@yandex.ru>
2023-07-30 00:51:20 +0300

    Add some tests

--- src/rust/ficus_backend/tests/utils/user_data_tests.rs
+++ src/rust/ficus_backend/tests/utils/user_data_tests.rs
@@ -1,5 +1,6 @@
-use ficus_backend::utils::user_data::{Key, UserData};
+use std::rc::Rc;
 
+use ficus_backend::utils::user_data::{Key, UserData};
 
 #[test]
 fn test_user_data() {
@@ -14,4 +15,42 @@ fn test_user_data() {
     *value = 321;
 
     assert_eq!(*user_data.get(&key).unwrap(), 321);
+}
+
+#[test]
+fn test_user_data_two_keys() {
+    let first_key = Key::<Rc<Box<usize>>>::new(&"1".to_string());
+    let second_key = Key::<Rc<Box<usize>>>::new(&"2".to_string());
+
+    let first_value = Rc::new(Box::new(123));
+    let second_value = Rc::new(Box::new(321));
+    let box1 = Box::new(Rc::clone(&first_value));
+    let box2 = Box::new(Rc::clone(&second_value));
+
+    let mut user_data = UserData::new();
+
+    user_data.put(&first_key, box1);
+    user_data.put(&second_key, box2);
+
+    assert_eq!(user_data.get(&first_key).unwrap(), &first_value);
+    assert_eq!(user_data.get(&second_key).unwrap(), &second_value);
+
+    assert!(Rc::ptr_eq(user_data.get(&first_key).unwrap(), &first_value));
+    assert!(Rc::ptr_eq(user_data.get(&second_key).unwrap(), &second_value));
+}
+
+#[test]
+fn test_remove_from_user_data() {
+    let key = Key::<usize>::new(&"1".to_string());
+
+    let value = 123;
+    let mut user_data = UserData::new();
+
+    user_data.put(&key, Box::new(value));
+
+    assert_eq!(*user_data.get(&key).unwrap(), 123);
+
+    user_data.remove(&key);
+
+    assert_eq!(user_data.get(&key), None);
 }
