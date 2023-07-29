548addf2dd720dad7f26dc12bc57bbc573308f7d
aero <aerooneQ@yandex.ru>
2023-07-30 02:46:00 +0300

    Drop unnecessary generic constraints

--- src/rust/ficus_backend/src/event_log/core/log_iterator.rs
+++ src/rust/ficus_backend/src/event_log/core/log_iterator.rs
@@ -1,23 +1,21 @@
 use std::{cell::RefCell, rc::Rc};
 
-use super::{event::event::Event, event_log::EventLog, trace::trace::Trace};
+use super::{event_log::EventLog, trace::trace::Trace};
 
-pub struct LogIterator<TLog, TEvent, TEventProcessor, TResult>
+pub struct LogIterator<TLog, TEventProcessor, TResult>
 where
-    TEvent: Event,
-    TLog: EventLog<TEvent = TEvent>,
-    TEventProcessor: Fn(&TEvent) -> TResult,
+    TLog: EventLog,
+    TEventProcessor: Fn(&TLog::TEvent) -> TResult,
 {
     log: Rc<RefCell<TLog>>,
     event_processor: Rc<TEventProcessor>,
     trace_index: usize,
 }
 
-impl<TLog, TEvent, TEventProcessor, TResult> LogIterator<TLog, TEvent, TEventProcessor, TResult>
+impl<TLog, TEventProcessor, TResult> LogIterator<TLog, TEventProcessor, TResult>
 where
-    TEvent: Event,
-    TLog: EventLog<TEvent = TEvent>,
-    TEventProcessor: Fn(&TEvent) -> TResult,
+    TLog: EventLog,
+    TEventProcessor: Fn(&TLog::TEvent) -> TResult,
 {
     pub fn new(log: Rc<RefCell<TLog>>, event_processor: Rc<TEventProcessor>) -> Self {
         Self {
@@ -28,14 +26,12 @@ where
     }
 }
 
-impl<TLog, TTrace, TEvent, TEventProcessor, TResult> Iterator for LogIterator<TLog, TEvent, TEventProcessor, TResult>
+impl<TLog, TEventProcessor, TResult> Iterator for LogIterator<TLog, TEventProcessor, TResult>
 where
-    TEvent: Event,
-    TTrace: Trace<TEvent = TEvent>,
-    TLog: EventLog<TTrace = TTrace, TEvent = TEvent>,
-    TEventProcessor: Fn(&TEvent) -> TResult,
+    TLog: EventLog,
+    TEventProcessor: Fn(&TLog::TEvent) -> TResult,
 {
-    type Item = TraceIterator<TTrace, TEvent, TEventProcessor, TResult>;
+    type Item = TraceIterator<TLog::TTrace, TEventProcessor, TResult>;
 
     fn next(&mut self) -> Option<Self::Item> {
         let log = self.log.borrow();
@@ -57,20 +53,20 @@ where
     }
 }
 
-pub struct TraceIterator<TTrace, TEvent, TEventProcessor, TResult>
+pub struct TraceIterator<TTrace, TEventProcessor, TResult>
 where
-    TTrace: Trace<TEvent = TEvent>,
-    TEventProcessor: Fn(&TEvent) -> TResult,
+    TTrace: Trace,
+    TEventProcessor: Fn(&TTrace::TEvent) -> TResult,
 {
     trace: Rc<RefCell<TTrace>>,
     event_processor: Rc<TEventProcessor>,
     event_index: usize,
 }
 
-impl<TTrace, TEvent, TEventProcessor, TResult> Iterator for TraceIterator<TTrace, TEvent, TEventProcessor, TResult>
+impl<TTrace, TEventProcessor, TResult> Iterator for TraceIterator<TTrace, TEventProcessor, TResult>
 where
-    TTrace: Trace<TEvent = TEvent>,
-    TEventProcessor: Fn(&TEvent) -> TResult,
+    TTrace: Trace,
+    TEventProcessor: Fn(&TTrace::TEvent) -> TResult,
 {
     type Item = TResult;
 
@@ -88,10 +84,10 @@ where
     }
 }
 
-impl<TTrace, TEvent, TEventProcessor, TResult> TraceIterator<TTrace, TEvent, TEventProcessor, TResult>
+impl<TTrace, TEventProcessor, TResult> TraceIterator<TTrace, TEventProcessor, TResult>
 where
-    TTrace: Trace<TEvent = TEvent>,
-    TEventProcessor: Fn(&TEvent) -> TResult,
+    TTrace: Trace,
+    TEventProcessor: Fn(&TTrace::TEvent) -> TResult,
 {
     pub fn new(trace: Rc<RefCell<TTrace>>, event_processor: Rc<TEventProcessor>) -> Self {
         Self {
