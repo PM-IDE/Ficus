f314d20f0426de82bb91ba62a4dc5180f89b5327
aero <aerooneQ@yandex.ru>
2023-08-09 23:46:05 +0300

    Fix API

--- src/rust/ficus_backend/src/grpc/backend_service.rs
+++ src/rust/ficus_backend/src/grpc/backend_service.rs
@@ -1,7 +1,7 @@
 use tonic::{Request, Response, Status};
 
 use crate::ficus_proto::{
-    backend_service_server::BackendService, ContextKey, ContextValue, GrpcGuid, PipelineExecutionRequest,
+    backend_service_server::BackendService, ContextValue, GrpcGuid, PipelineExecutionRequest, GetContextValueRequest,
 };
 
 pub struct FicusService {}
@@ -9,10 +9,10 @@ pub struct FicusService {}
 #[tonic::async_trait]
 impl BackendService for FicusService {
     async fn execute_pipeline(&self, request: Request<PipelineExecutionRequest>) -> Result<Response<GrpcGuid>, Status> {
-        todo!()
+        todo!();
     }
 
-    async fn get_context_value(&self, request: Request<ContextKey>) -> Result<Response<ContextValue>, Status> {
-        todo!()
+    async fn get_context_value(&self, request: Request<GetContextValueRequest>) -> Result<Response<ContextValue>, Status> {
+        todo!();
     }
 }
