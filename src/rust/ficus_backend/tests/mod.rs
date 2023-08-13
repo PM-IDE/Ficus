0d40cca5f773073d8b8427ec89280014e46af6b3
aero <aerooneQ@yandex.ru>
2023-08-13 13:04:04 +0300

    Fix tests

--- src/rust/ficus_backend/tests/mod.rs
+++ src/rust/ficus_backend/tests/mod.rs
@@ -1,7 +1,7 @@
 pub mod analysis;
 pub mod interval_tree;
 pub mod other;
+pub mod pipelines;
 pub mod suffix_tree;
 pub mod test_core;
 pub mod utils;
-pub mod pipelines;
--- src/rust/ficus_backend/tests/pipelines/pipeline_keys_tests.rs
+++ src/rust/ficus_backend/tests/pipelines/pipeline_keys_tests.rs
@@ -1,7 +1,15 @@
 use std::sync::Arc;
 
-use ficus_backend::{pipelines::{keys::context_keys::ContextKeys, context::PipelineContext, aliases::{Activities, ActivitiesToLogs, Patterns, RepeatSets, TracesActivities}}, event_log::{xes::xes_event_log::XesEventLogImpl, core::event_log::EventLog}, features::discovery::petri_net::PetriNet};
-
+use ficus_backend::{
+    event_log::{core::event_log::EventLog, xes::xes_event_log::XesEventLogImpl},
+    features::discovery::petri_net::PetriNet,
+    pipelines::{
+        aliases::{Activities, ActivitiesToLogs, Patterns, RepeatSets, TracesActivities},
+        context::PipelineContext,
+        keys::context_keys::ContextKeys,
+    },
+    utils::user_data::Key,
+};
 
 #[test]
 fn test_event_log_key() {
@@ -25,6 +33,7 @@ fn execute_test(test: impl Fn(&ContextKeys, &mut PipelineContext) -> ()) {
 }
 
 #[test]
+#[rustfmt::skip]
 fn test_event_log_all_concrete_keys() {
     execute_test(|keys, _| {
         assert!(keys.find_concrete_key::<XesEventLogImpl>(ContextKeys::EVENT_LOG).is_some());
@@ -42,6 +51,7 @@ fn test_event_log_all_concrete_keys() {
 }
 
 #[test]
+#[rustfmt::skip]
 fn test_event_log_all_keys() {
     execute_test(|keys, _| {
         assert!(keys.find_key(ContextKeys::EVENT_LOG).is_some());
@@ -55,5 +65,23 @@ fn test_event_log_all_keys() {
         assert!(keys.find_key(ContextKeys::REPEAT_SETS).is_some());
         assert!(keys.find_key(ContextKeys::TRACE_ACTIVITIES).is_some());
         assert!(keys.find_key(ContextKeys::PATH).is_some());
+    })
+}
+
+#[test]
+#[rustfmt::skip]
+fn test_equivalence_of_keys() {
+    execute_test(|keys, _| {
+        assert!(keys.find_key(ContextKeys::EVENT_LOG).unwrap().key().id() == keys.find_concrete_key::<XesEventLogImpl>(ContextKeys::EVENT_LOG).unwrap().key().id());
+        assert!(keys.find_key(ContextKeys::ACTIVITIES).unwrap().key().id() == keys.find_concrete_key::<Activities>(ContextKeys::ACTIVITIES).unwrap().key().id());
+        assert!(keys.find_key(ContextKeys::ACTIVITIES_TO_LOGS).unwrap().key().id() == keys.find_concrete_key::<ActivitiesToLogs>(ContextKeys::ACTIVITIES_TO_LOGS).unwrap().key().id());
+        assert!(keys.find_key(ContextKeys::ACTIVITY_NAME).unwrap().key().id() == keys.find_concrete_key::<String>(ContextKeys::ACTIVITY_NAME).unwrap().key().id());
+        assert!(keys.find_key(ContextKeys::PATTERNS).unwrap().key().id() == keys.find_concrete_key::<Patterns>(ContextKeys::PATTERNS).unwrap().key().id());
+        assert!(keys.find_key(ContextKeys::HASHES_EVENT_LOG).unwrap().key().id() == keys.find_concrete_key::<Vec<Vec<u64>>>(ContextKeys::HASHES_EVENT_LOG).unwrap().key().id());
+        assert!(keys.find_key(ContextKeys::NAMES_EVENT_LOG).unwrap().key().id() == keys.find_concrete_key::<Vec<Vec<String>>>(ContextKeys::NAMES_EVENT_LOG).unwrap().key().id());
+        assert!(keys.find_key(ContextKeys::PETRI_NET).unwrap().key().id() == keys.find_concrete_key::<PetriNet>(ContextKeys::PETRI_NET).unwrap().key().id());
+        assert!(keys.find_key(ContextKeys::REPEAT_SETS).unwrap().key().id() == keys.find_concrete_key::<RepeatSets>(ContextKeys::REPEAT_SETS).unwrap().key().id());
+        assert!(keys.find_key(ContextKeys::TRACE_ACTIVITIES).unwrap().key().id() == keys.find_concrete_key::<TracesActivities>(ContextKeys::TRACE_ACTIVITIES).unwrap().key().id());
+        assert!(keys.find_key(ContextKeys::PATH).unwrap().key().id() == keys.find_concrete_key::<String>(ContextKeys::PATH).unwrap().key().id());
     })
 }
