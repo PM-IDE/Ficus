cd5e0f5106e8c59804392ea36dc4a783c7692698
aero <aerooneQ@yandex.ru>
2023-09-11 14:42:36 +0300

    Refactored tests

--- src/rust/ficus_backend/tests/pipelines/pipeline_keys_tests.rs
+++ src/rust/ficus_backend/tests/pipelines/pipeline_keys_tests.rs
@@ -3,8 +3,11 @@ use std::sync::Arc;
 use ficus_backend::{
     event_log::{core::event_log::EventLog, xes::xes_event_log::XesEventLogImpl},
     features::{
-        analysis::patterns::{
-            activity_instances::AdjustingMode, contexts::PatternsDiscoveryStrategy, entry_points::PatternsKind,
+        analysis::{
+            event_log_info::EventLogInfo,
+            patterns::{
+                activity_instances::AdjustingMode, contexts::PatternsDiscoveryStrategy, entry_points::PatternsKind,
+            },
         },
         discovery::petri_net::PetriNet,
     },
@@ -46,34 +49,45 @@ fn execute_test(test: impl Fn(&ContextKeys, &mut PipelineContext) -> ()) {
 #[rustfmt::skip]
 fn test_event_log_all_concrete_keys() {
     execute_test(|keys, _| {
-        assert!(keys.find_concrete_key::<XesEventLogImpl>(ContextKeys::EVENT_LOG).is_some());
-        assert!(keys.find_concrete_key::<Activities>(ContextKeys::ACTIVITIES).is_some());
-        assert!(keys.find_concrete_key::<ActivitiesToLogs>(ContextKeys::ACTIVITIES_TO_LOGS).is_some());
-        assert!(keys.find_concrete_key::<String>(ContextKeys::ACTIVITY_NAME).is_some());
-        assert!(keys.find_concrete_key::<Patterns>(ContextKeys::PATTERNS).is_some());
-        assert!(keys.find_concrete_key::<Vec<Vec<u64>>>(ContextKeys::HASHES_EVENT_LOG).is_some());
-        assert!(keys.find_concrete_key::<Vec<Vec<String>>>(ContextKeys::NAMES_EVENT_LOG).is_some());
-        assert!(keys.find_concrete_key::<PetriNet>(ContextKeys::PETRI_NET).is_some());
-        assert!(keys.find_concrete_key::<RepeatSets>(ContextKeys::REPEAT_SETS).is_some());
-        assert!(keys.find_concrete_key::<TracesActivities>(ContextKeys::TRACE_ACTIVITIES).is_some());
-        assert!(keys.find_concrete_key::<String>(ContextKeys::PATH).is_some());
-        assert!(keys.find_concrete_key::<ColorsEventLog>(ContextKeys::COLORS_EVENT_LOG).is_some());
-        assert!(keys.find_concrete_key::<ColorsHolder>(ContextKeys::COLORS_HOLDER).is_some());
-        assert!(keys.find_concrete_key::<usize>(ContextKeys::UNDERLYING_EVENTS_COUNT).is_some());
-        assert!(keys.find_concrete_key::<u32>(ContextKeys::EVENTS_COUNT).is_some());
-        assert!(keys.find_concrete_key::<Vec<String>>(ContextKeys::EVENT_CLASSES_REGEXES).is_some());
-        assert!(keys.find_concrete_key::<AdjustingMode>(ContextKeys::ADJUSTING_MODE).is_some());
-        assert!(keys.find_concrete_key::<String>(ContextKeys::EVENT_CLASS_REGEX).is_some());
-        assert!(keys.find_concrete_key::<PatternsKind>(ContextKeys::PATTERNS_KIND).is_some());
-        assert!(keys.find_concrete_key::<Pipeline>(ContextKeys::PIPELINE).is_some());
+        let mut count = 0usize;
 
-        assert!(keys.find_concrete_key::<u32>(ContextKeys::TANDEM_ARRAY_LENGTH).is_some());
-        assert!(keys.find_concrete_key::<u32>(ContextKeys::ACTIVITY_LEVEL).is_some());
-        assert!(keys.find_concrete_key::<bool>(ContextKeys::NARROW_ACTIVITIES).is_some());
-        assert!(keys.find_concrete_key::<String>(ContextKeys::EVENT_NAME).is_some());
-        assert!(keys.find_concrete_key::<String>(ContextKeys::REGEX).is_some());
-        assert!(keys.find_concrete_key::<PatternsDiscoveryStrategy>(ContextKeys::PATTERNS_DISCOVERY_STRATEGY).is_some());
+        assert_existence::<String>(keys, ContextKeys::PATH, &mut count);
+        assert_existence::<u32>(keys, ContextKeys::TANDEM_ARRAY_LENGTH, &mut count);
+        assert_existence::<u32>(keys, ContextKeys::ACTIVITY_LEVEL, &mut count);
+        assert_existence::<bool>(keys, ContextKeys::NARROW_ACTIVITIES, &mut count);
+        assert_existence::<String>(keys, ContextKeys::EVENT_NAME, &mut count);
+        assert_existence::<String>(keys, ContextKeys::REGEX, &mut count);
+        assert_existence::<PatternsDiscoveryStrategy>(keys, ContextKeys::PATTERNS_DISCOVERY_STRATEGY, &mut count);
+        assert_existence::<String>(keys, ContextKeys::OUTPUT_STRING, &mut count);
+        assert_existence::<EventLogInfo>(keys, ContextKeys::EVENT_LOG_INFO, &mut count);
+        assert_existence::<usize>(keys, ContextKeys::UNDERLYING_EVENTS_COUNT, &mut count);
+        assert_existence::<u32>(keys, ContextKeys::EVENTS_COUNT, &mut count);
+        assert_existence::<Vec<String>>(keys, ContextKeys::EVENT_CLASSES_REGEXES, &mut count);
+        assert_existence::<AdjustingMode>(keys, ContextKeys::ADJUSTING_MODE, &mut count);
+        assert_existence::<String>(keys, ContextKeys::EVENT_CLASS_REGEX, &mut count);
+        assert_existence::<PatternsKind>(keys, ContextKeys::PATTERNS_KIND, &mut count);
+        assert_existence::<Pipeline>(keys, ContextKeys::PIPELINE, &mut count);
+
+        assert_existence::<XesEventLogImpl>(keys, ContextKeys::EVENT_LOG, &mut count);
+        assert_existence::<Activities>(keys, ContextKeys::ACTIVITIES, &mut count);
+        assert_existence::<ActivitiesToLogs>(keys, ContextKeys::ACTIVITIES_TO_LOGS, &mut count);
+        assert_existence::<String>(keys, ContextKeys::ACTIVITY_NAME, &mut count);
+        assert_existence::<Patterns>(keys, ContextKeys::PATTERNS, &mut count);
+        assert_existence::<Vec<Vec<u64>>>(keys, ContextKeys::HASHES_EVENT_LOG, &mut count);
+        assert_existence::<Vec<Vec<String>>>(keys, ContextKeys::NAMES_EVENT_LOG, &mut count);
+        assert_existence::<PetriNet>(keys, ContextKeys::PETRI_NET, &mut count);
+        assert_existence::<RepeatSets>(keys, ContextKeys::REPEAT_SETS, &mut count);
+        assert_existence::<TracesActivities>(keys, ContextKeys::TRACE_ACTIVITIES, &mut count);
+        assert_existence::<ColorsEventLog>(keys, ContextKeys::COLORS_EVENT_LOG, &mut count);
+        assert_existence::<ColorsHolder>(keys, ContextKeys::COLORS_HOLDER, &mut count);
+
+        assert_eq!(count, get_all_keys_names().len())
     })
+}
+
+fn assert_existence<T: 'static>(keys: &ContextKeys, name: &str, count: &mut usize) {
+    assert!(keys.find_concrete_key::<T>(name).is_some());
+    *count += 1;
 }
 
 #[rustfmt::skip]
@@ -129,31 +143,44 @@ fn test_keys_count() {
 #[rustfmt::skip]
 fn test_equivalence_of_keys() {
     execute_test(|keys, _| {
-        assert!(keys.find_key(ContextKeys::EVENT_LOG).unwrap().key().id() == keys.find_concrete_key::<XesEventLogImpl>(ContextKeys::EVENT_LOG).unwrap().key().id());
-        assert!(keys.find_key(ContextKeys::ACTIVITIES).unwrap().key().id() == keys.find_concrete_key::<Activities>(ContextKeys::ACTIVITIES).unwrap().key().id());
-        assert!(keys.find_key(ContextKeys::ACTIVITIES_TO_LOGS).unwrap().key().id() == keys.find_concrete_key::<ActivitiesToLogs>(ContextKeys::ACTIVITIES_TO_LOGS).unwrap().key().id());
-        assert!(keys.find_key(ContextKeys::ACTIVITY_NAME).unwrap().key().id() == keys.find_concrete_key::<String>(ContextKeys::ACTIVITY_NAME).unwrap().key().id());
-        assert!(keys.find_key(ContextKeys::PATTERNS).unwrap().key().id() == keys.find_concrete_key::<Patterns>(ContextKeys::PATTERNS).unwrap().key().id());
-        assert!(keys.find_key(ContextKeys::HASHES_EVENT_LOG).unwrap().key().id() == keys.find_concrete_key::<Vec<Vec<u64>>>(ContextKeys::HASHES_EVENT_LOG).unwrap().key().id());
-        assert!(keys.find_key(ContextKeys::NAMES_EVENT_LOG).unwrap().key().id() == keys.find_concrete_key::<Vec<Vec<String>>>(ContextKeys::NAMES_EVENT_LOG).unwrap().key().id());
-        assert!(keys.find_key(ContextKeys::PETRI_NET).unwrap().key().id() == keys.find_concrete_key::<PetriNet>(ContextKeys::PETRI_NET).unwrap().key().id());
-        assert!(keys.find_key(ContextKeys::REPEAT_SETS).unwrap().key().id() == keys.find_concrete_key::<RepeatSets>(ContextKeys::REPEAT_SETS).unwrap().key().id());
-        assert!(keys.find_key(ContextKeys::TRACE_ACTIVITIES).unwrap().key().id() == keys.find_concrete_key::<TracesActivities>(ContextKeys::TRACE_ACTIVITIES).unwrap().key().id());
-        assert!(keys.find_key(ContextKeys::PATH).unwrap().key().id() == keys.find_concrete_key::<String>(ContextKeys::PATH).unwrap().key().id());
-        assert!(keys.find_key(ContextKeys::TANDEM_ARRAY_LENGTH).unwrap().key().id() == keys.find_concrete_key::<u32>(ContextKeys::TANDEM_ARRAY_LENGTH).unwrap().key().id());
-        assert!(keys.find_key(ContextKeys::ACTIVITY_LEVEL).unwrap().key().id() == keys.find_concrete_key::<u32>(ContextKeys::ACTIVITY_LEVEL).unwrap().key().id());
-        assert!(keys.find_key(ContextKeys::NARROW_ACTIVITIES).unwrap().key().id() == keys.find_concrete_key::<bool>(ContextKeys::NARROW_ACTIVITIES).unwrap().key().id());
-        assert!(keys.find_key(ContextKeys::EVENT_NAME).unwrap().key().id() == keys.find_concrete_key::<String>(ContextKeys::EVENT_NAME).unwrap().key().id());
-        assert!(keys.find_key(ContextKeys::REGEX).unwrap().key().id() == keys.find_concrete_key::<String>(ContextKeys::REGEX).unwrap().key().id());
-        assert!(keys.find_key(ContextKeys::COLORS_EVENT_LOG).unwrap().key().id() == keys.find_concrete_key::<ColorsEventLog>(ContextKeys::COLORS_EVENT_LOG).unwrap().key().id());
-        assert!(keys.find_key(ContextKeys::COLORS_HOLDER).unwrap().key().id() == keys.find_concrete_key::<ColorsHolder>(ContextKeys::COLORS_HOLDER).unwrap().key().id());
-        assert!(keys.find_key(ContextKeys::PATTERNS_DISCOVERY_STRATEGY).unwrap().key().id() == keys.find_concrete_key::<PatternsDiscoveryStrategy>(ContextKeys::PATTERNS_DISCOVERY_STRATEGY).unwrap().key().id());
-        assert!(keys.find_key(ContextKeys::UNDERLYING_EVENTS_COUNT).unwrap().key().id() == keys.find_concrete_key::<usize>(ContextKeys::UNDERLYING_EVENTS_COUNT).unwrap().key().id());
-        assert!(keys.find_key(ContextKeys::EVENTS_COUNT).unwrap().key().id() == keys.find_concrete_key::<u32>(ContextKeys::EVENTS_COUNT).unwrap().key().id());
-        assert!(keys.find_key(ContextKeys::EVENT_CLASSES_REGEXES).unwrap().key().id() == keys.find_concrete_key::<Vec<String>>(ContextKeys::EVENT_CLASSES_REGEXES).unwrap().key().id());
-        assert!(keys.find_key(ContextKeys::ADJUSTING_MODE).unwrap().key().id() == keys.find_concrete_key::<AdjustingMode>(ContextKeys::ADJUSTING_MODE).unwrap().key().id());
-        assert!(keys.find_key(ContextKeys::EVENT_CLASS_REGEX).unwrap().key().id() == keys.find_concrete_key::<String>(ContextKeys::EVENT_CLASS_REGEX).unwrap().key().id());
-        assert!(keys.find_key(ContextKeys::PATTERNS_KIND).unwrap().key().id() == keys.find_concrete_key::<PatternsKind>(ContextKeys::PATTERNS_KIND).unwrap().key().id());
-        assert!(keys.find_key(ContextKeys::PIPELINE).unwrap().key().id() == keys.find_concrete_key::<Pipeline>(ContextKeys::PIPELINE).unwrap().key().id());
+        let mut count = 0;
+
+        assert_keys_equivalence::<String>(keys, ContextKeys::PATH, &mut count);        
+        assert_keys_equivalence::<u32>(keys, ContextKeys::TANDEM_ARRAY_LENGTH, &mut count);        
+        assert_keys_equivalence::<u32>(keys, ContextKeys::ACTIVITY_LEVEL, &mut count);        
+        assert_keys_equivalence::<bool>(keys, ContextKeys::NARROW_ACTIVITIES, &mut count);        
+        assert_keys_equivalence::<String>(keys, ContextKeys::EVENT_NAME, &mut count);        
+        assert_keys_equivalence::<String>(keys, ContextKeys::REGEX, &mut count);        
+        assert_keys_equivalence::<ColorsEventLog>(keys, ContextKeys::COLORS_EVENT_LOG, &mut count);        
+        assert_keys_equivalence::<ColorsHolder>(keys, ContextKeys::COLORS_HOLDER, &mut count);        
+        assert_keys_equivalence::<PatternsDiscoveryStrategy>(keys, ContextKeys::PATTERNS_DISCOVERY_STRATEGY, &mut count);       
+        assert_keys_equivalence::<String>(keys, ContextKeys::OUTPUT_STRING, &mut count);         
+        assert_keys_equivalence::<EventLogInfo>(keys, ContextKeys::EVENT_LOG_INFO, &mut count);
+        assert_keys_equivalence::<usize>(keys, ContextKeys::UNDERLYING_EVENTS_COUNT, &mut count);        
+        assert_keys_equivalence::<u32>(keys, ContextKeys::EVENTS_COUNT, &mut count);        
+        assert_keys_equivalence::<Vec<String>>(keys, ContextKeys::EVENT_CLASSES_REGEXES, &mut count);        
+        assert_keys_equivalence::<AdjustingMode>(keys, ContextKeys::ADJUSTING_MODE, &mut count);        
+        assert_keys_equivalence::<String>(keys, ContextKeys::EVENT_CLASS_REGEX, &mut count);        
+        assert_keys_equivalence::<PatternsKind>(keys, ContextKeys::PATTERNS_KIND, &mut count);        
+        assert_keys_equivalence::<Pipeline>(keys, ContextKeys::PIPELINE, &mut count);
+
+        assert_keys_equivalence::<XesEventLogImpl>(keys, ContextKeys::EVENT_LOG, &mut count);
+        assert_keys_equivalence::<Activities>(keys, ContextKeys::ACTIVITIES, &mut count);
+        assert_keys_equivalence::<ActivitiesToLogs>(keys, ContextKeys::ACTIVITIES_TO_LOGS, &mut count);        
+        assert_keys_equivalence::<String>(keys, ContextKeys::ACTIVITY_NAME, &mut count);        
+        assert_keys_equivalence::<Patterns>(keys, ContextKeys::PATTERNS, &mut count);        
+        assert_keys_equivalence::<Vec<Vec<u64>>>(keys, ContextKeys::HASHES_EVENT_LOG, &mut count);        
+        assert_keys_equivalence::<Vec<Vec<String>>>(keys, ContextKeys::NAMES_EVENT_LOG, &mut count);        
+        assert_keys_equivalence::<PetriNet>(keys, ContextKeys::PETRI_NET, &mut count);        
+        assert_keys_equivalence::<RepeatSets>(keys, ContextKeys::REPEAT_SETS, &mut count);        
+        assert_keys_equivalence::<TracesActivities>(keys, ContextKeys::TRACE_ACTIVITIES, &mut count);        
+        
+
+        assert_eq!(count, get_all_keys_names().len())
     })
+}
+
+fn assert_keys_equivalence<T: 'static>(keys: &ContextKeys, name: &str, count: &mut usize) {
+    assert!(keys.find_key(name).unwrap().key().id() == keys.find_concrete_key::<T>(name).unwrap().key().id());
+    *count += 1;
 }
