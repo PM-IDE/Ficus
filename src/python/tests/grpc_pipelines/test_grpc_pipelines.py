44247c0a0c4c9212e637353bb715fe896ba3217c
aero <aerooneQ@yandex.ru>
2023-09-14 16:48:02 +0300

    More refactorings

--- src/python/tests/grpc_pipelines/test_grpc_pipelines.py
+++ src/python/tests/grpc_pipelines/test_grpc_pipelines.py
@@ -2,7 +2,7 @@ from ficus.grpc_pipelines.activities_parts import DiscoverActivities2, DiscoverA
     CreateLogFromActivitiesInstances2, DiscoverActivitiesForSeveralLevels2, DiscoverActivitiesUntilNoMore2, \
     DiscoverActivitiesFromPatterns2
 from ficus.grpc_pipelines.constants import const_names_event_log
-from ficus.grpc_pipelines.context_values import StringContextValue, NamesLogContextValue
+from ficus.grpc_pipelines.context_values import StringContextValue, NamesLogContextValue, ContextValue
 from ficus.grpc_pipelines.data_models import PatternsKind
 from ficus.grpc_pipelines.drawing_parts import TracesDiversityDiagram2, DrawPlacementsOfEventByName2, \
     DrawPlacementOfEventsByRegex2
@@ -31,14 +31,15 @@ def test_simple_pipeline():
 
 
 def test_pipeline_with_getting_context_value():
-    pipeline = Pipeline2(
+    _execute_test_with_exercise_log('exercise1', Pipeline2(
         ReadLogFromXes2(),
         TracesDiversityDiagram2(),
-    )
+    ))
 
-    log_path = get_example_log_path('exercise1.xes')
+
+def _execute_test_with_exercise_log(log_name: str, pipeline: Pipeline2):
     result = pipeline.execute({
-        'path': StringContextValue(log_path)
+        'path': StringContextValue(get_example_log_path(f'{log_name}.xes'))
     })
 
     assert result.finalResult.HasField('success')
@@ -46,18 +47,10 @@ def test_pipeline_with_getting_context_value():
 
 
 def test_pipeline_with_getting_context_value2():
-    pipeline = Pipeline2(
+    _execute_test_with_exercise_log('exercise1', Pipeline2(
         ReadLogFromXes2(),
         DrawPlacementsOfEventByName2('A'),
-    )
-
-    log_path = get_example_log_path('exercise1.xes')
-    result = pipeline.execute({
-        'path': StringContextValue(log_path)
-    })
-
-    assert result.finalResult.HasField('success')
-    assert not result.finalResult.HasField('error')
+    ))
 
 
 def test_pipeline_with_getting_context_value3():
@@ -65,18 +58,10 @@ def test_pipeline_with_getting_context_value3():
 
 
 def _do_simple_test_with_regex(regex: str):
-    pipeline = Pipeline2(
+    _execute_test_with_exercise_log('exercise1', Pipeline2(
         ReadLogFromXes2(),
         DrawPlacementOfEventsByRegex2(regex)
-    )
-
-    log_path = get_example_log_path('exercise1.xes')
-    result = pipeline.execute({
-        'path': StringContextValue(log_path)
-    })
-
-    assert result.finalResult.HasField('success')
-    assert not result.finalResult.HasField('error')
+    ))
 
 
 def test_pipeline_with_getting_context_value4():
@@ -88,7 +73,7 @@ def test_pipeline_with_getting_context_value5():
 
 
 def test_draw_short_activities_diagram():
-    pipeline = Pipeline2(
+    _execute_test_with_exercise_log('exercise4', Pipeline2(
         ReadLogFromXes2(),
         FindSuperMaximalRepeats2(strategy=PatternsDiscoveryStrategy.FromSingleMergedTrace),
         DiscoverActivities2(activity_level=0),
@@ -102,19 +87,11 @@ def test_draw_short_activities_diagram():
             ['(a)::(b)', '(c)::(d)', 'f'],
             ['(a)::(b)', '(e)', '(b)::(d)', 'f']
         ])
-    )
-
-    log_path = get_example_log_path('exercise4.xes')
-    result = pipeline.execute({
-        'path': StringContextValue(log_path)
-    })
-
-    assert result.finalResult.HasField('success')
-    assert not result.finalResult.HasField('error')
+    ))
 
 
 def test_draw_full_activities_diagram_2():
-    pipeline = Pipeline2(
+    _execute_test_with_exercise_log('exercise4', Pipeline2(
         ReadLogFromXes2(),
         FindSuperMaximalRepeats2(strategy=PatternsDiscoveryStrategy.FromAllTraces),
         DiscoverActivities2(activity_level=0),
@@ -128,34 +105,18 @@ def test_draw_full_activities_diagram_2():
             ['a', 'b', 'c', 'd', 'f'],
             ['a', 'e', 'd', 'f']
         ])
-    )
-
-    log_path = get_example_log_path('exercise4.xes')
-    result = pipeline.execute({
-        'path': StringContextValue(log_path)
-    })
-
-    assert result.finalResult.HasField('success')
-    assert not result.finalResult.HasField('error')
+    ))
 
 
 def test_get_event_log_info():
-    pipeline = Pipeline2(
+    _execute_test_with_exercise_log('exercise4', Pipeline2(
         ReadLogFromXes2(),
         PrintEventLogInfo2(),
-    )
-
-    log_path = get_example_log_path('exercise4.xes')
-    result = pipeline.execute({
-        'path': StringContextValue(log_path)
-    })
-
-    assert result.finalResult.HasField('success')
-    assert not result.finalResult.HasField('error')
+    ))
 
 
 def test_filter_traces_by_events_count():
-    pipeline = Pipeline2(
+    _execute_test_with_exercise_log('exercise4', Pipeline2(
         ReadLogFromXes2(),
         FilterTracesByEventsCount2(min_events_in_trace=5),
         AssertNamesLogTestPart([
@@ -164,59 +125,51 @@ def test_filter_traces_by_events_count():
             ['a', 'c', 'd', 'b', 'f'],
             ['a', 'b', 'c', 'd', 'f']
         ])
-    )
-
-    log_path = get_example_log_path('exercise4.xes')
-    result = pipeline.execute({
-        'path': StringContextValue(log_path)
-    })
-
-    assert result.finalResult.HasField('success')
-    assert not result.finalResult.HasField('error')
+    ))
 
 
 def test_class_extractors():
-    pipeline = Pipeline2(
-        UseNamesEventLog2(),
-        FindSuperMaximalRepeats2(strategy=PatternsDiscoveryStrategy.FromAllTraces, class_extractor='^(.*?)\\.'),
-        DiscoverActivities2(activity_level=0),
-        DiscoverActivitiesInstances2(narrow_activities=True),
-        CreateLogFromActivitiesInstances2(),
-        AssertNamesLogTestPart([
-            ['(A.A)::(B.B)', 'C', '(A.A)::(B.B)'],
-            ['(A.A)::(B.B)', 'C', '(A.A)::(B.B)']
-        ])
-    )
-
-    result = pipeline.execute({
-        const_names_event_log: NamesLogContextValue([
+    _execute_test_with_names_log(
+        [
             ['A.A', 'B.B', 'C', 'A.C', 'B.D'],
             ['A.D', 'B.C', 'C', 'A.A', 'B.B'],
-        ])
-    })
-
-    assert result.finalResult.HasField('success')
-    assert not result.finalResult.HasField('error')
+        ],
+        Pipeline2(
+            UseNamesEventLog2(),
+            FindSuperMaximalRepeats2(strategy=PatternsDiscoveryStrategy.FromAllTraces, class_extractor='^(.*?)\\.'),
+            DiscoverActivities2(activity_level=0),
+            DiscoverActivitiesInstances2(narrow_activities=True),
+            CreateLogFromActivitiesInstances2(),
+            AssertNamesLogTestPart([
+                ['(A.A)::(B.B)', 'C', '(A.A)::(B.B)'],
+                ['(A.A)::(B.B)', 'C', '(A.A)::(B.B)']
+            ])
+    ))
 
 
 def test_several_levels():
-    pipeline = Pipeline2(
-        UseNamesEventLog2(),
-        TracesDiversityDiagram2(plot_legend=True, title='InitialLog'),
-        DiscoverActivitiesForSeveralLevels2(event_classes=['^(.*?)\\.', '.*'],
-                                            patterns_kind=PatternsKind.MaximalRepeats),
-        CreateLogFromActivitiesInstances2(),
-        AssertNamesLogTestPart([
-            ['(A.D)::(B.C)', '(C)::(D)', '(A.D)::(B.C)', '(C)::(D)'],
-            ['(A.D)::(B.C)', '(C)::(D)', '(A.D)::(B.C)']
-        ])
-    )
-
-    result = pipeline.execute({
-        const_names_event_log: NamesLogContextValue([
+    _execute_test_with_names_log(
+        [
             ['A.A', 'B.B', 'C', 'D', 'A.C', 'B.D', 'C', 'D'],
             ['A.D', 'B.C', 'C', 'D', 'A.A', 'B.B'],
-        ])
+        ],
+        Pipeline2(
+            UseNamesEventLog2(),
+            TracesDiversityDiagram2(plot_legend=True, title='InitialLog'),
+            DiscoverActivitiesForSeveralLevels2(event_classes=['^(.*?)\\.', '.*'],
+                                                patterns_kind=PatternsKind.MaximalRepeats),
+            CreateLogFromActivitiesInstances2(),
+            AssertNamesLogTestPart([
+                ['(A.D)::(B.C)', '(C)::(D)', '(A.D)::(B.C)', '(C)::(D)'],
+                ['(A.D)::(B.C)', '(C)::(D)', '(A.D)::(B.C)']
+            ])
+        )
+    )
+
+
+def _execute_test_with_names_log(names_log: list[list[str]], pipeline: Pipeline2):
+    result = pipeline.execute({
+        const_names_event_log: NamesLogContextValue(names_log)
     })
 
     assert result.finalResult.HasField('success')
@@ -224,45 +177,37 @@ def test_several_levels():
 
 
 def test_discover_activities_from_patterns():
-    pipeline = Pipeline2(
-        UseNamesEventLog2(),
-        DiscoverActivitiesFromPatterns2(patterns_kind=PatternsKind.MaximalRepeats),
-        DiscoverActivitiesInstances2(narrow_activities=True),
-        CreateLogFromActivitiesInstances2(),
-        AssertNamesLogTestPart([['(A)::(B)', 'C', '(A)::(B)'], ['(A)::(B)', 'C', '(A)::(B)']])
+    _execute_test_with_names_log(
+        [
+            ['A', 'B', 'C', 'A', 'B'],
+            ['A', 'B', 'C', 'A', 'B'],
+        ],
+        Pipeline2(
+            UseNamesEventLog2(),
+            DiscoverActivitiesFromPatterns2(patterns_kind=PatternsKind.MaximalRepeats),
+            DiscoverActivitiesInstances2(narrow_activities=True),
+            CreateLogFromActivitiesInstances2(),
+            AssertNamesLogTestPart([['(A)::(B)', 'C', '(A)::(B)'], ['(A)::(B)', 'C', '(A)::(B)']])
+        )
     )
-
-    result = pipeline.execute({
-        const_names_event_log: NamesLogContextValue([
-            ['A', 'B', 'C', 'A', 'B'],
-            ['A', 'B', 'C', 'A', 'B'],
-        ])
-    })
-
-    assert result.finalResult.HasField('success')
-    assert not result.finalResult.HasField('error')
 
 
 def test_discover_activities_until_no_more():
-    pipeline = Pipeline2(
-        UseNamesEventLog2(),
-        DiscoverActivitiesUntilNoMore2(event_class='^(.*?)\\.'),
-        AssertNamesLogTestPart([['(A.A)::(B.B)::(C)::(D)'], ['(A.A)::(B.B)::(C)::(D)']])
-    )
-
-    result = pipeline.execute({
-        const_names_event_log: NamesLogContextValue([
+    _execute_test_with_names_log(
+        [
             ['A.A', 'B.B', 'C', 'D', 'A.C', 'B.D', 'C', 'D'],
             ['A.D', 'B.C', 'C', 'D', 'A.A', 'B.B'],
-        ])
-    })
-
-    assert result.finalResult.HasField('success')
-    assert not result.finalResult.HasField('error')
+        ],
+        Pipeline2(
+            UseNamesEventLog2(),
+            DiscoverActivitiesUntilNoMore2(event_class='^(.*?)\\.'),
+            AssertNamesLogTestPart([['(A.A)::(B.B)::(C)::(D)'], ['(A.A)::(B.B)::(C)::(D)']])
+        )
+    )
 
 
 def test_filter_events_by_name():
-    pipeline = Pipeline2(
+    _execute_test_with_exercise_log('exercise4', Pipeline2(
         ReadLogFromXes2(),
         FilterEventsByName2(event_name='a'),
         AssertNamesLogTestPart([
@@ -273,19 +218,11 @@ def test_filter_events_by_name():
             ['b', 'c', 'd', 'f'],
             ['e', 'd', 'f']
         ])
-    )
-
-    log_path = get_example_log_path('exercise4.xes')
-    result = pipeline.execute({
-        'path': StringContextValue(log_path)
-    })
-
-    assert result.finalResult.HasField('success')
-    assert not result.finalResult.HasField('error')
+    ))
 
 
 def test_filter_events_by_regex():
-    pipeline = Pipeline2(
+    _execute_test_with_exercise_log('exercise4', Pipeline2(
         ReadLogFromXes2(),
         FilterEventsByRegex2(regex='a|b'),
         AssertNamesLogTestPart([
@@ -296,19 +233,11 @@ def test_filter_events_by_regex():
             ['c', 'd', 'f'],
             ['e', 'd', 'f']
         ])
-    )
-
-    log_path = get_example_log_path('exercise4.xes')
-    result = pipeline.execute({
-        'path': StringContextValue(log_path)
-    })
-
-    assert result.finalResult.HasField('success')
-    assert not result.finalResult.HasField('error')
+    ))
 
 
 def test_filter_log_by_variants():
-    pipeline = Pipeline2(
+    _execute_test_with_exercise_log('exercise4', Pipeline2(
         ReadLogFromXes2(),
         FilterLogByVariants2(),
         AssertNamesLogTestPart([
@@ -319,12 +248,4 @@ def test_filter_log_by_variants():
             ['a', 'b', 'c', 'd', 'f'],
             ['a', 'e', 'd', 'f']
         ])
-    )
-
-    log_path = get_example_log_path('exercise4.xes')
-    result = pipeline.execute({
-        'path': StringContextValue(log_path)
-    })
-
-    assert result.finalResult.HasField('success')
-    assert not result.finalResult.HasField('error')
+    ))
