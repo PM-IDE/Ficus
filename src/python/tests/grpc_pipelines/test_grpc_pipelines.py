34b60309c9077f01cd6e6f7fd8f8fd0480b5ff6c
aero <aerooneQ@yandex.ru>
2023-10-12 23:30:16 +0300

    Add some tests

--- src/python/tests/grpc_pipelines/test_grpc_pipelines.py
+++ src/python/tests/grpc_pipelines/test_grpc_pipelines.py
@@ -429,6 +429,39 @@ def test_discover_petri_net_alpha5():
     )
 
 
+def test_discover_petri_net_alpha6():
+    _execute_discovery_test(
+        'test_discover_petri_net_alpha6',
+        [
+            ['A', 'B', 'E', 'C', 'C', 'D'],
+            ['A', 'B', 'C', 'C', 'E', 'D'],
+            ['A', 'E', 'C', 'C', 'D'],
+        ],
+        DiscoverPetriNetAlpha2()
+    )
+
+def test_discover_petri_net_alpha7():
+    _execute_discovery_test(
+        'test_discover_petri_net_alpha7',
+        [
+            ['A', 'B', 'B', 'C', 'C', 'D'],
+            ['A', 'C', 'C', 'B', 'B', 'D'],
+            ['A', 'C', 'C', 'D'],
+        ],
+        DiscoverPetriNetAlpha2()
+    )
+
+def test_discover_petri_net_alpha_plus8():
+    _execute_discovery_test(
+        'test_discover_petri_net_alpha8',
+        [
+            ['A', 'B', 'B', 'D'],
+            ['A', 'B', 'C', 'C', 'B', 'D'],
+            ['A', 'B', 'C', 'C', 'C', 'C', 'B', 'D'],
+        ],
+        DiscoverPetriNetAlpha2()
+    )
+
 def test_discover_petri_net_alpha_plus():
     _execute_discovery_test(
         'test_discover_petri_net_alpha_plus',
@@ -474,6 +507,39 @@ def test_discover_petri_net_alpha_plus5():
             ['A', 'B', 'C', 'D'],
             ['A', 'C', 'B', 'D'],
             ['A', 'E', 'D'],
+        ],
+        DiscoverPetriNetAlphaPlus2()
+    )
+
+def test_discover_petri_net_alpha_plus6():
+    _execute_discovery_test(
+        'test_discover_petri_net_alpha6',
+        [
+            ['A', 'B', 'E', 'C', 'C', 'D'],
+            ['A', 'B', 'C', 'C', 'E', 'D'],
+            ['A', 'E', 'C', 'C', 'D'],
+        ],
+        DiscoverPetriNetAlphaPlus2()
+    )
+
+def test_discover_petri_net_alpha_plus7():
+    _execute_discovery_test(
+        'test_discover_petri_net_alpha7',
+        [
+            ['A', 'B', 'B', 'C', 'C', 'D'],
+            ['A', 'C', 'C', 'B', 'B', 'D'],
+            ['A', 'C', 'C', 'D'],
+        ],
+        DiscoverPetriNetAlphaPlus2()
+    )
+
+def test_discover_petri_net_alpha_plus8():
+    _execute_discovery_test(
+        'test_discover_petri_net_alpha8',
+        [
+            ['A', 'B', 'B', 'D'],
+            ['A', 'B', 'C', 'C', 'B', 'D'],
+            ['A', 'B', 'C', 'C', 'C', 'C', 'B', 'D'],
         ],
         DiscoverPetriNetAlphaPlus2()
     )
