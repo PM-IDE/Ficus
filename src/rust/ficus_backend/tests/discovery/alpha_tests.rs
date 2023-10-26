712f83f00734384c7d78b671990f709e44075a95
aero <aerooneQ@yandex.ru>
2023-10-26 13:45:30 +0300

    Fix tests

--- src/rust/ficus_backend/tests/discovery/alpha_tests.rs
+++ src/rust/ficus_backend/tests/discovery/alpha_tests.rs
@@ -10,6 +10,7 @@ use ficus_backend::features::analysis::event_log_info::{EventLogInfo, EventLogIn
 use ficus_backend::features::discovery::alpha::alpha::discover_petri_net_alpha;
 use ficus_backend::features::discovery::alpha::alpha_plus_plus_nfc::alpha_plus_plus_nfc::discover_petri_net_alpha_plus_plus_nfc;
 use ficus_backend::features::discovery::alpha::alpha_sharp::discover_petri_net_alpha_sharp;
+use ficus_backend::features::discovery::alpha::providers::alpha_provider::DefaultAlphaRelationsProvider;
 use ficus_backend::features::discovery::petri_net::pnml_serialization::serialize_to_pnml;
 
 #[test]
@@ -17,8 +18,9 @@ pub fn alpha_simple_test_1() {
     execute_test_with_gold(get_serialized_petri_nets_gold_path("alpha_simple_test_1"), || {
         let log = create_simple_event_log3();
         let info = EventLogInfo::create_from(EventLogInfoCreationDto::default(&log));
+        let provider = DefaultAlphaRelationsProvider::new(&info);
 
-        serialize_to_pnml(&discover_petri_net_alpha(&info), true).ok().unwrap()
+        serialize_to_pnml(&discover_petri_net_alpha(&provider), true).ok().unwrap()
     })
 }
 
