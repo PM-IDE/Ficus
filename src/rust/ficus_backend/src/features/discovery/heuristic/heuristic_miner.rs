006e61fd9fabb99daebca6985e92477a583fc36f
aero <aerooneQ@yandex.ru>
2023-11-02 00:48:57 +0300

    Fix overflow bug

--- src/rust/ficus_backend/src/features/discovery/heuristic/heuristic_miner.rs
+++ src/rust/ficus_backend/src/features/discovery/heuristic/heuristic_miner.rs
@@ -80,7 +80,7 @@ impl<'a> HeuristicMinerMeasureProvider<'a> {
             .dfg_info()
             .get_directly_follows_count(&(second.to_owned(), first.to_owned()));
 
-        let measure = (b_follows_a - a_follows_b) as f64 / (b_follows_a + a_follows_b + 1) as f64;
+        let measure = (b_follows_a as f64 - a_follows_b as f64)/ (b_follows_a as f64 + a_follows_b as f64 + 1.0);
         measure > self.dependency_threshold
     }
 
