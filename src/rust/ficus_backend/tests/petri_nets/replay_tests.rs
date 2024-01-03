5cc91b597b5bcb4b17e0a53b7bd6307494c577a1
aero <aerooneQ@yandex.ru>
2024-01-03 23:26:20 +0300

    Better replay tests

--- src/rust/ficus_backend/tests/petri_nets/replay_tests.rs
+++ src/rust/ficus_backend/tests/petri_nets/replay_tests.rs
@@ -1,13 +1,13 @@
-use ficus_backend::features::{
+use ficus_backend::{features::{
     analysis::event_log_info::{EventLogInfo, EventLogInfoCreationDto},
     discovery::{
         alpha::{alpha::discover_petri_net_alpha, providers::alpha_provider::DefaultAlphaRelationsProvider},
         petri_net::{
             annotations::{annotate_with_counts, annotate_with_frequencies},
-            replay::replay_petri_net,
+            replay::replay_petri_net, petri_net::DefaultPetriNet,
         },
     },
-};
+}, event_log::core::event_log::EventLog, vecs};
 
 use crate::test_core::simple_events_logs_provider::create_simple_event_log;
 
@@ -17,7 +17,35 @@ pub fn test_simple_replay() {
     let log_info = EventLogInfo::create_from(EventLogInfoCreationDto::default(&log));
     let petri_net = discover_petri_net_alpha(&DefaultAlphaRelationsProvider::new(&log_info));
 
-    println!("{:?}", replay_petri_net(&log, &petri_net));
+    let expected_transitions = vec![
+        Some(vecs!["A", "B", "C"]),
+        Some(vecs!["A", "B", "C"]),
+    ];
+
+    execute_test_with_replay(&petri_net, &log, expected_transitions);
+}
+
+fn execute_test_with_replay(net: &DefaultPetriNet, log: &impl EventLog, expected_transitions: Vec<Option<Vec<String>>>) {
+    let replay_states = replay_petri_net(log, net).unwrap();
+    if replay_states.len() != expected_transitions.len() {
+        panic!();
+    }
+
+    for (replay_state, expected_transitions) in replay_states.iter().zip(expected_transitions.iter()) {
+        if replay_state.is_none() && expected_transitions.is_none() {
+            continue;
+        }
+
+        if !(replay_state.is_some() && expected_transitions.is_some()) {
+            panic!();
+        }
+
+        let expected = expected_transitions.as_ref().unwrap();
+        let state = replay_state.as_ref().unwrap();
+
+        let replayed_transitions: Vec<String> = state.fired_transitions().iter().map(|id| net.transition(id).name().to_owned()).collect();
+        assert_eq!(&replayed_transitions, expected);
+    }
 }
 
 #[test]
