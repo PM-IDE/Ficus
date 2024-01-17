9baac563f3de1600469b59bc4dd1105cf66e20fd
aero <aerooneQ@yandex.ru>
2024-01-17 22:42:42 +0300

    Create proper dataset for levenshtein distance

--- src/rust/ficus_backend/src/features/analysis/patterns/clustering/dbscan.rs
+++ src/rust/ficus_backend/src/features/analysis/patterns/clustering/dbscan.rs
@@ -10,7 +10,7 @@ use crate::{event_log::core::{event_log::EventLog, trace::trace::Trace, event::e
 use super::{
     common::{create_dataset, transform_to_ficus_dataset, create_colors_vector, MyDataset, scale_raw_dataset_min_max},
     merging::merge_activities,
-    params::{ActivitiesClusteringParams, DistanceWrapper, TracesClusteringParams},
+    params::{ActivitiesClusteringParams, DistanceWrapper, TracesClusteringParams, FicusDistance},
 };
 
 pub fn clusterize_activities_dbscan<TLog: EventLog>(
@@ -53,7 +53,7 @@ pub fn clusterize_log_by_traces_dbscan<TLog: EventLog>(
     params: &mut TracesClusteringParams<TLog>,
     min_points: usize,
 ) -> Option<(Vec<TLog>, LabeledDataset)> {
-    if let Some((dataset, objects, features)) = create_traces_dataset(params.vis_params.log) {
+    if let Some((dataset, objects, features)) = create_traces_dataset(params.vis_params.log, &params.distance) {
         let clusters = Dbscan::params_with(min_points, DistanceWrapper::new(params.distance), KdTree)
             .tolerance(params.tolerance)
             .transform(dataset.records())
@@ -89,7 +89,14 @@ pub fn clusterize_log_by_traces_dbscan<TLog: EventLog>(
     }
 }
 
-fn create_traces_dataset<TLog: EventLog>(log: &TLog) -> Option<(MyDataset, Vec<String>, Vec<String>)> {
+fn create_traces_dataset<TLog: EventLog>(log: &TLog, distance: &FicusDistance) -> Option<(MyDataset, Vec<String>, Vec<String>)> {
+    match distance {
+        FicusDistance::Cosine | FicusDistance::L1 | FicusDistance::L2 => create_traces_dataset_default(log),
+        FicusDistance::Levenshtein => create_traces_dataset_levenshtein(log),
+    }
+}
+
+fn create_traces_dataset_default<TLog: EventLog>(log: &TLog) -> Option<(MyDataset, Vec<String>, Vec<String>)> {
     let mut all_event_classes = HashSet::new();
     for trace in log.traces() {
         let trace = trace.borrow();
@@ -131,5 +138,44 @@ fn create_traces_dataset<TLog: EventLog>(log: &TLog) -> Option<(MyDataset, Vec<S
         DatasetBase::from(array),
         (0..log.traces().len()).into_iter().map(|x| format!("Trace_{}", x)).collect(),
         all_event_classes,
+    ))
+}
+
+fn create_traces_dataset_levenshtein<TLog: EventLog>(log: &TLog) -> Option<(MyDataset, Vec<String>, Vec<String>)> {
+    let mut all_event_classes = HashMap::new();
+    let mut max_length = 0;
+    for trace in log.traces() {
+        let trace = trace.borrow();
+        max_length = max_length.max(trace.events().len() + 1);
+
+        for event in trace.events() {
+            if !all_event_classes.contains_key(event.borrow().name()) {
+                all_event_classes.insert(event.borrow().name().to_owned(), all_event_classes.len());
+            }
+        }
+    }
+
+    let mut raw_dataset = vec![];
+    for trace in log.traces() {
+        let trace = trace.borrow();
+        for event in trace.events() {
+            raw_dataset.push(*all_event_classes.get(event.borrow().name()).expect("Should be there") as f64);
+        }
+
+        for _ in trace.events().len()..max_length {
+            raw_dataset.push(0f64);
+        }
+    }
+
+    let shape = (log.traces().len(), max_length);
+    let array = match Array2::from_shape_vec(shape, raw_dataset) {
+        Ok(score) => score,
+        Err(_) => return None,
+    };
+
+    Some((
+        DatasetBase::from(array),
+        (0..log.traces().len()).into_iter().map(|x| format!("Trace_{}", x)).collect(),
+        (0..max_length).into_iter().map(|x| format!("Symbol_{}", x)).collect(),
     ))
 }
