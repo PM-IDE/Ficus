aacdca299007bdf2a7d6785da4f43af35611c98e
aero <aerooneQ@yandex.ru>
2024-02-06 13:15:10 +0300

    Meaningless refactorings

--- src/rust/ficus_backend/src/event_log/core/event/lifecycle/braf_lifecycle.rs
+++ src/rust/ficus_backend/src/event_log/core/event/lifecycle/braf_lifecycle.rs
@@ -24,29 +24,50 @@ pub enum XesBrafLifecycle {
     OpenRunningSuspended = 19,
 }
 
+const UNSPECIFIED: &'static str = "Unspecified";
+const CLOSED: &'static str = "Closed";
+const CLOSED_CANCELLED: &'static str = "Closed.Cancelled";
+const CLOSED_CANCELLED_ABORTED: &'static str = "Closed.Cancelled.Aborted";
+const CLOSED_CANCELLED_ERROR: &'static str = "Closed.Cancelled.Error";
+const CLOSED_CANCELLED_EXITED: &'static str = "Closed.Cancelled.Exited";
+const CLOSED_CANCELLED_OBSOLETE: &'static str = "Closed.Cancelled.Obsolete";
+const CLOSED_CANCELLED_TERMINATED: &'static str = "Closed.Cancelled.Terminated";
+const COMPLETED: &'static str = "Completed";
+const COMPLETED_FAILED: &'static str = "Completed.Failed";
+const COMPLETED_SUCCESS: &'static str = "Completed.Success";
+const OPEN: &'static str = "Open";
+const OPEN_NOTRUNNING: &'static str = "Open.NotRunning";
+const OPEN_NOTRUNNING_ASSIGNED: &'static str = "Open.NotRunning.Assigned";
+const OPEN_NOTRUNNING_RESERVED: &'static str = "Open.NotRunning.Reserved";
+const OPEN_NOTRUNNING_SUSPENDED_ASSIGNED: &'static str = "Open.NotRunning.Suspended.Assigned";
+const OPEN_NOTRUNNING_SUSPENDED_RESERVED: &'static str = "Open.NotRunning.Suspended.Reserved";
+const OPEN_RUNNING: &'static str = "Open.Running";
+const OPEN_RUNNING_INPROGRESS: &'static str = "Open.Running.InProgress";
+const OPEN_RUNNING_SUSPENDED: &'static str = "Open.Running.Suspended";
+
 impl ToString for XesBrafLifecycle {
     fn to_string(&self) -> String {
         match self {
-            XesBrafLifecycle::Unspecified => String::from_str("Unspecified").ok().unwrap(),
-            XesBrafLifecycle::Closed => String::from_str("Closed").ok().unwrap(),
-            XesBrafLifecycle::ClosedCancelled => String::from_str("Closed.Cancelled").ok().unwrap(),
-            XesBrafLifecycle::ClosedCancelledAborted => String::from_str("Closed.Cancelled.Aborted").ok().unwrap(),
-            XesBrafLifecycle::ClosedCancelledError => String::from_str("Closed.Cancelled.Error").ok().unwrap(),
-            XesBrafLifecycle::ClosedCancelledExited => String::from_str("Closed.Cancelled.Exited").ok().unwrap(),
-            XesBrafLifecycle::ClosedCancelledObsolete => String::from_str("Closed.Cancelled.Obsolete").ok().unwrap(),
-            XesBrafLifecycle::ClosedCancelledTerminated => String::from_str("Closed.Cancelled.Terminated").ok().unwrap(),
-            XesBrafLifecycle::Completed => String::from_str("Completed").ok().unwrap(),
-            XesBrafLifecycle::CompletedFailed => String::from_str("Completed.Failed").ok().unwrap(),
-            XesBrafLifecycle::CompletedSuccess => String::from_str("Completed.Success").ok().unwrap(),
-            XesBrafLifecycle::Open => String::from_str("Open").ok().unwrap(),
-            XesBrafLifecycle::OpenNotRunning => String::from_str("Open.NotRunning").ok().unwrap(),
-            XesBrafLifecycle::OpenNotRunningAssigned => String::from_str("Open.NotRunning.Assigned").ok().unwrap(),
-            XesBrafLifecycle::OpenNotRunningReserved => String::from_str("Open.NotRunning.Reserved").ok().unwrap(),
-            XesBrafLifecycle::OpenNotRunningSuspendedAssigned => String::from_str("Open.NotRunning.Suspended.Assigned").ok().unwrap(),
-            XesBrafLifecycle::OpenNotRunningSuspendedReserved => String::from_str("Open.NotRunning.Suspended.Reserved").ok().unwrap(),
-            XesBrafLifecycle::OpenRunning => String::from_str("Open.Running").ok().unwrap(),
-            XesBrafLifecycle::OpenRunningInProgress => String::from_str("Open.Running.InProgress").ok().unwrap(),
-            XesBrafLifecycle::OpenRunningSuspended => String::from_str("Open.Running.Suspended").ok().unwrap(),
+            XesBrafLifecycle::Unspecified => String::from_str(UNSPECIFIED).ok().unwrap(),
+            XesBrafLifecycle::Closed => String::from_str(CLOSED).ok().unwrap(),
+            XesBrafLifecycle::ClosedCancelled => String::from_str(CLOSED_CANCELLED).ok().unwrap(),
+            XesBrafLifecycle::ClosedCancelledAborted => String::from_str(CLOSED_CANCELLED_ABORTED).ok().unwrap(),
+            XesBrafLifecycle::ClosedCancelledError => String::from_str(CLOSED_CANCELLED_ERROR).ok().unwrap(),
+            XesBrafLifecycle::ClosedCancelledExited => String::from_str(CLOSED_CANCELLED_EXITED).ok().unwrap(),
+            XesBrafLifecycle::ClosedCancelledObsolete => String::from_str(CLOSED_CANCELLED_OBSOLETE).ok().unwrap(),
+            XesBrafLifecycle::ClosedCancelledTerminated => String::from_str(CLOSED_CANCELLED_TERMINATED).ok().unwrap(),
+            XesBrafLifecycle::Completed => String::from_str(COMPLETED).ok().unwrap(),
+            XesBrafLifecycle::CompletedFailed => String::from_str(COMPLETED_FAILED).ok().unwrap(),
+            XesBrafLifecycle::CompletedSuccess => String::from_str(COMPLETED_SUCCESS).ok().unwrap(),
+            XesBrafLifecycle::Open => String::from_str(OPEN).ok().unwrap(),
+            XesBrafLifecycle::OpenNotRunning => String::from_str(OPEN_NOTRUNNING).ok().unwrap(),
+            XesBrafLifecycle::OpenNotRunningAssigned => String::from_str(OPEN_NOTRUNNING_ASSIGNED).ok().unwrap(),
+            XesBrafLifecycle::OpenNotRunningReserved => String::from_str(OPEN_NOTRUNNING_RESERVED).ok().unwrap(),
+            XesBrafLifecycle::OpenNotRunningSuspendedAssigned => String::from_str(OPEN_NOTRUNNING_SUSPENDED_ASSIGNED).ok().unwrap(),
+            XesBrafLifecycle::OpenNotRunningSuspendedReserved => String::from_str(OPEN_NOTRUNNING_SUSPENDED_RESERVED).ok().unwrap(),
+            XesBrafLifecycle::OpenRunning => String::from_str(OPEN_RUNNING).ok().unwrap(),
+            XesBrafLifecycle::OpenRunningInProgress => String::from_str(OPEN_RUNNING_INPROGRESS).ok().unwrap(),
+            XesBrafLifecycle::OpenRunningSuspended => String::from_str(OPEN_RUNNING_SUSPENDED).ok().unwrap(),
         }
     }
 }
@@ -56,26 +77,26 @@ impl FromStr for XesBrafLifecycle {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s {
-            "Unspecified" => Ok(XesBrafLifecycle::Unspecified),
-            "Closed" => Ok(XesBrafLifecycle::Closed),
-            "Closed.Cancelled" => Ok(XesBrafLifecycle::ClosedCancelled),
-            "Closed.Cancelled.Aborted" => Ok(XesBrafLifecycle::ClosedCancelledAborted),
-            "Closed.Cancelled.Error" => Ok(XesBrafLifecycle::ClosedCancelledError),
-            "Closed.Cancelled.Exited" => Ok(XesBrafLifecycle::ClosedCancelledExited),
-            "Closed.Cancelled.Obsolete" => Ok(XesBrafLifecycle::ClosedCancelledObsolete),
-            "Closed.Cancelled.Terminated" => Ok(XesBrafLifecycle::ClosedCancelledTerminated),
-            "Completed" => Ok(XesBrafLifecycle::Completed),
-            "Completed.Failed" => Ok(XesBrafLifecycle::CompletedFailed),
-            "Completed.Success" => Ok(XesBrafLifecycle::CompletedSuccess),
-            "Open" => Ok(XesBrafLifecycle::Open),
-            "Open.NotRunning" => Ok(XesBrafLifecycle::OpenNotRunning),
-            "Open.NotRunning.Assigned" => Ok(XesBrafLifecycle::OpenNotRunningAssigned),
-            "Open.NotRunning.Reserved" => Ok(XesBrafLifecycle::OpenNotRunningReserved),
-            "Open.NotRunning.Suspended.Assigned" => Ok(XesBrafLifecycle::OpenNotRunningSuspendedAssigned),
-            "Open.NotRunning.Suspended.Reserved" => Ok(XesBrafLifecycle::OpenNotRunningSuspendedReserved),
-            "Open.Running" => Ok(XesBrafLifecycle::OpenRunning),
-            "Open.Running.InProgress" => Ok(XesBrafLifecycle::OpenRunningInProgress),
-            "Open.Running.Suspended" => Ok(XesBrafLifecycle::OpenRunningSuspended),
+            UNSPECIFIED => Ok(XesBrafLifecycle::Unspecified),
+            CLOSED => Ok(XesBrafLifecycle::Closed),
+            CLOSED_CANCELLED => Ok(XesBrafLifecycle::ClosedCancelled),
+            CLOSED_CANCELLED_ABORTED => Ok(XesBrafLifecycle::ClosedCancelledAborted),
+            CLOSED_CANCELLED_ERROR => Ok(XesBrafLifecycle::ClosedCancelledError),
+            CLOSED_CANCELLED_EXITED => Ok(XesBrafLifecycle::ClosedCancelledExited),
+            CLOSED_CANCELLED_OBSOLETE => Ok(XesBrafLifecycle::ClosedCancelledObsolete),
+            CLOSED_CANCELLED_TERMINATED => Ok(XesBrafLifecycle::ClosedCancelledTerminated),
+            COMPLETED => Ok(XesBrafLifecycle::Completed),
+            COMPLETED_FAILED => Ok(XesBrafLifecycle::CompletedFailed),
+            COMPLETED_SUCCESS => Ok(XesBrafLifecycle::CompletedSuccess),
+            OPEN => Ok(XesBrafLifecycle::Open),
+            OPEN_NOTRUNNING => Ok(XesBrafLifecycle::OpenNotRunning),
+            OPEN_NOTRUNNING_ASSIGNED => Ok(XesBrafLifecycle::OpenNotRunningAssigned),
+            OPEN_NOTRUNNING_RESERVED => Ok(XesBrafLifecycle::OpenNotRunningReserved),
+            OPEN_NOTRUNNING_SUSPENDED_ASSIGNED => Ok(XesBrafLifecycle::OpenNotRunningSuspendedAssigned),
+            OPEN_NOTRUNNING_SUSPENDED_RESERVED => Ok(XesBrafLifecycle::OpenNotRunningSuspendedReserved),
+            OPEN_RUNNING => Ok(XesBrafLifecycle::OpenRunning),
+            OPEN_RUNNING_INPROGRESS => Ok(XesBrafLifecycle::OpenRunningInProgress),
+            OPEN_RUNNING_SUSPENDED => Ok(XesBrafLifecycle::OpenRunningSuspended),
             _ => Err(()),
         }
     }
--- src/rust/ficus_backend/src/event_log/core/event/lifecycle/standard_lifecycle.rs
+++ src/rust/ficus_backend/src/event_log/core/event/lifecycle/standard_lifecycle.rs
@@ -1,26 +1,5 @@
 use std::str::FromStr;
 
-impl ToString for XesStandardLifecycle {
-    fn to_string(&self) -> String {
-        match self {
-            Self::Schedule => String::from_str("schedule").ok().unwrap(),
-            Self::Start => String::from_str("start").ok().unwrap(),
-            Self::Complete => String::from_str("complete").ok().unwrap(),
-            Self::Unknown => String::from_str("unknown").ok().unwrap(),
-            Self::Unspecified => String::from_str("unspecified").ok().unwrap(),
-            Self::Assign => String::from_str("assign").ok().unwrap(),
-            Self::AteAbort => String::from_str("ate_abort").ok().unwrap(),
-            Self::Autoskip => String::from_str("autoskip").ok().unwrap(),
-            Self::ManualSkip => String::from_str("manualskip").ok().unwrap(),
-            Self::PiAbort => String::from_str("pi_abort").ok().unwrap(),
-            Self::ReAssign => String::from_str("reassign").ok().unwrap(),
-            Self::Resume => String::from_str("resume").ok().unwrap(),
-            Self::Suspend => String::from_str("suspend").ok().unwrap(),
-            Self::Withdraw => String::from_str("withdraw").ok().unwrap(),
-        }
-    }
-}
-
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum XesStandardLifecycle {
     Unspecified = 0,
@@ -39,25 +18,61 @@ pub enum XesStandardLifecycle {
     Withdraw = 13,
 }
 
+const SCHEDULE: &'static str = "schedule";
+const START: &'static str = "start";
+const COMPLETE: &'static str = "complete";
+const UNKNOWN: &'static str = "unknown";
+const UNSPECIFIED: &'static str = "unspecified";
+const ASSIGN: &'static str = "assign";
+const ATE_ABORT: &'static str = "ate_abort";
+const AUTOSKIP: &'static str = "autoskip";
+const MANUAL_SKIP: &'static str = "manualskip";
+const PI_ABORT: &'static str = "pi_abort";
+const RE_ASSIGN: &'static str = "reassign";
+const RESUME: &'static str = "resume";
+const SUSPEND: &'static str = "suspend";
+const WITHDRAW: &'static str = "withdraw";
+
+impl ToString for XesStandardLifecycle {
+    fn to_string(&self) -> String {
+        match self {
+            Self::Schedule => String::from_str(SCHEDULE).ok().unwrap(),
+            Self::Start => String::from_str(START).ok().unwrap(),
+            Self::Complete => String::from_str(COMPLETE).ok().unwrap(),
+            Self::Unknown => String::from_str(UNKNOWN).ok().unwrap(),
+            Self::Unspecified => String::from_str(UNSPECIFIED).ok().unwrap(),
+            Self::Assign => String::from_str(ASSIGN).ok().unwrap(),
+            Self::AteAbort => String::from_str(ATE_ABORT).ok().unwrap(),
+            Self::Autoskip => String::from_str(AUTOSKIP).ok().unwrap(),
+            Self::ManualSkip => String::from_str(MANUAL_SKIP).ok().unwrap(),
+            Self::PiAbort => String::from_str(PI_ABORT).ok().unwrap(),
+            Self::ReAssign => String::from_str(RE_ASSIGN).ok().unwrap(),
+            Self::Resume => String::from_str(RESUME).ok().unwrap(),
+            Self::Suspend => String::from_str(SUSPEND).ok().unwrap(),
+            Self::Withdraw => String::from_str(WITHDRAW).ok().unwrap(),
+        }
+    }
+}
+
 impl FromStr for XesStandardLifecycle {
     type Err = ParseXesStandardLifecycleError;
 
     fn from_str(s: &str) -> Result<XesStandardLifecycle, Self::Err> {
         match s {
-            "schedule" => Ok(XesStandardLifecycle::Schedule),
-            "start" => Ok(XesStandardLifecycle::Start),
-            "complete" => Ok(XesStandardLifecycle::Complete),
-            "unknown" => Ok(XesStandardLifecycle::Unknown),
-            "unspecified" => Ok(XesStandardLifecycle::Unspecified),
-            "assign" => Ok(XesStandardLifecycle::Assign),
-            "ate_abort" => Ok(XesStandardLifecycle::AteAbort),
-            "autoskip" => Ok(XesStandardLifecycle::Autoskip),
-            "manualskip" => Ok(XesStandardLifecycle::ManualSkip),
-            "pi_abort" => Ok(XesStandardLifecycle::PiAbort),
-            "reassign" => Ok(XesStandardLifecycle::ReAssign),
-            "suspend" => Ok(XesStandardLifecycle::Suspend),
-            "resume" => Ok(XesStandardLifecycle::Resume),
-            "withdraw" => Ok(XesStandardLifecycle::Withdraw),
+            SCHEDULE => Ok(XesStandardLifecycle::Schedule),
+            START => Ok(XesStandardLifecycle::Start),
+            COMPLETE => Ok(XesStandardLifecycle::Complete),
+            UNKNOWN => Ok(XesStandardLifecycle::Unknown),
+            UNSPECIFIED => Ok(XesStandardLifecycle::Unspecified),
+            ASSIGN => Ok(XesStandardLifecycle::Assign),
+            ATE_ABORT => Ok(XesStandardLifecycle::AteAbort),
+            AUTOSKIP => Ok(XesStandardLifecycle::Autoskip),
+            MANUAL_SKIP => Ok(XesStandardLifecycle::ManualSkip),
+            PI_ABORT => Ok(XesStandardLifecycle::PiAbort),
+            RE_ASSIGN => Ok(XesStandardLifecycle::ReAssign),
+            SUSPEND => Ok(XesStandardLifecycle::Suspend),
+            RESUME => Ok(XesStandardLifecycle::Resume),
+            WITHDRAW => Ok(XesStandardLifecycle::Withdraw),
             _ => Err(ParseXesStandardLifecycleError),
         }
     }
