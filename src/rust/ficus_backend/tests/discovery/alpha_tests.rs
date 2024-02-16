56c4326e20a0cf45ebface75a30f4d8cb0305412
aero <aerooneQ@yandex.ru>
2024-02-16 14:53:24 +0300

    Fix some tests

--- src/rust/ficus_backend/tests/discovery/alpha_tests.rs
+++ src/rust/ficus_backend/tests/discovery/alpha_tests.rs
@@ -3,17 +3,21 @@ use crate::test_core::simple_events_logs_provider::{
     create_alpha_plus_plus_nfc_test_log9, create_alpha_sharp_test_log2, create_simple_event_log3,
 };
 use crate::test_core::test_paths::get_serialized_petri_nets_gold_path;
+use ficus_backend::event_log::simple::simple_event_log::SimpleEventLog;
 use ficus_backend::features::analysis::event_log_info::{EventLogInfo, EventLogInfoCreationDto};
 use ficus_backend::features::discovery::alpha::alpha::discover_petri_net_alpha;
 use ficus_backend::features::discovery::alpha::alpha_plus_plus_nfc::alpha_plus_plus_nfc::discover_petri_net_alpha_plus_plus_nfc;
-use ficus_backend::features::discovery::alpha::alpha_sharp::discover_petri_net_alpha_sharp;
 use ficus_backend::features::discovery::alpha::providers::alpha_provider::DefaultAlphaRelationsProvider;
 use ficus_backend::features::discovery::petri_net::pnml_serialization::serialize_to_pnml;
 
 #[test]
 pub fn alpha_simple_test_1() {
-    execute_test_with_gold(get_serialized_petri_nets_gold_path("alpha_simple_test_1"), || {
-        let log = create_simple_event_log3();
+    execute_alpha_discovery_test("alpha_simple_test_1", || create_simple_event_log3());
+}
+
+fn execute_alpha_discovery_test(test_name: &str, log_creator: impl Fn() -> SimpleEventLog) {
+    execute_test_with_gold(get_serialized_petri_nets_gold_path(test_name), || {
+        let log = log_creator();
         let info = EventLogInfo::create_from(EventLogInfoCreationDto::default(&log));
         let provider = DefaultAlphaRelationsProvider::new(&info);
 
@@ -22,13 +26,13 @@ pub fn alpha_simple_test_1() {
 }
 
 #[test]
-pub fn alpha_sharp_test() {
-    let log = create_alpha_sharp_test_log2();
-    discover_petri_net_alpha_sharp(&log);
+pub fn alpha_plus_plus_nfc_test_1() {
+    execute_alpha_plus_plus_nfc_discovery_test("alpha_plus_plus_nfc_test_1", || create_alpha_plus_plus_nfc_test_log9());
 }
 
-#[test]
-pub fn alpha_plus_plus_nfc_test() {
-    let log = create_alpha_plus_plus_nfc_test_log9();
-    discover_petri_net_alpha_plus_plus_nfc(&log);
+fn execute_alpha_plus_plus_nfc_discovery_test(test_name: &str, log_creator: impl Fn() -> SimpleEventLog) {
+    execute_test_with_gold(get_serialized_petri_nets_gold_path(test_name), || {
+        let log = log_creator();
+        serialize_to_pnml(&discover_petri_net_alpha_plus_plus_nfc(&log), true).ok().unwrap()
+    })
 }
